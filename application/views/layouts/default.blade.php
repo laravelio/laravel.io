1497e67f9181f8923c4df6e03316f853d39f7f6e
Shawn McCool <shawn@heybigname.com>
2013-05-08 17:47:04 +0200

    added laracon copy below the logo

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -66,6 +66,9 @@
                 <a href="http://laracon.eu/">
                     <img src="http://laracon.eu/img/laracon-logo.png" alt="Larcon 2013 Amsterdam">
                 </a>
+                <p>
+                    Laracon Europe is a two-day event in historic Amsterdam where you'll meet many members of the Laravel community and listen to engaging presentations about the art of software engineering and more.
+                </p>
             </p>
             @render('layouts._featured_books')
         </aside>
