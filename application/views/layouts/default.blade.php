1ad2a97ee530c3e48c22beb03b7b9a1a70840896
Shawn McCool <shawn@heybigname.com>
2013-05-10 16:19:03 +0200

    updated url to laracon logo

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -65,7 +65,7 @@
         <aside>
             <p class="laraconlogo">
                 <a href="http://laracon.eu/">
-                    <img src="http://laracon.eu/img/laracon-logo.png" alt="Larcon 2013 Amsterdam">
+                    <img src="http://laracon.eu/2013/img/laracon-logo.png" alt="Larcon 2013 Amsterdam">
                 </a>
                 <p>
                     Laracon Europe is a two-day event in historic Amsterdam where you'll meet many members of the Laravel community and listen to engaging presentations about the art of software engineering and more.
