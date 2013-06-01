50a4885c702d6b5025158eec97926c5f7e062c7e
Shawn McCool <shawn@heybigname.com>
2013-06-01 18:38:16 +0200

    updated irc page

--- application/views/irc/index.php
+++ application/views/irc/index.php
@@ -2,4 +2,15 @@
 
 <iframe src="https://kiwiirc.com/client/irc.freenode.net/?&theme=basic#laravel" style="border:0; width:100%; height:450px;"></iframe>
 
-<a href="http://irclogs.laravel.io" target="_NEW">Search the chat logs</a> to see if your question has already been answered.
+<a href="http://irclogs.laravel.io" target="_NEW">Search the chat logs</a> to see if your question has already been answered. You can use your own IRC client at Freenode.net in #Laravel.
+
+<hr/>
+
+<p>
+<a href="http://laracon.eu/">
+    <img src="http://laracon.eu/2013/img/laracon-logo.png" alt="Larcon 2013 Amsterdam">
+</a>
+</p>
+<p style="width:300px">
+    Laracon Europe is a two-day event in historic Amsterdam where you'll meet many members of the Laravel community and listen to engaging presentations about the art of software engineering and more.
+</p>
