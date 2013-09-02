fb9d673c159fa8a94c4f988c264cd256070736f6
Mitchell van Wijngaarden <mitchell.wijngaarden@gmail.com>
2013-09-02 15:15:17 +0200

    Added Role model, Modified User model, Made repositories for both

--- app/domain/LIO/Accounts/User.php
+++ app/domain/LIO/Accounts/User.php
@@ -2,50 +2,31 @@
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
-
 use Eloquent;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+    const STATE_ACTIVE  = 1;
+    const STATE_BLOCKED = 2;
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = array('password');
+    protected $table  = 'users';
+    protected $hidden = ['password'];
 
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
+    public function roles()
+    {
+        return $this->belongsToMany('Lio\Accounts\Role');
+    }
+
     public function getAuthIdentifier()
     {
         return $this->getKey();
     }
 
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
     public function getAuthPassword()
     {
         return $this->password;
     }
 
-    /**
-     * Get the e-mail address where password reminders are sent.
-     *
-     * @return string
-     */
     public function getReminderEmail()
     {
         return $this->email;
