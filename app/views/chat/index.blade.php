d59356fb422e8edc94cbe84eaade08817f5d3de3
Shawn McCool <shawn@heybigname.com>
2013-09-20 18:16:39 +0200

    made chat annoying

--- app/views/chat/index.blade.php
+++ app/views/chat/index.blade.php
@@ -1,9 +1,17 @@
 <h2>#Laravel IRC Chat</h2>
 
-@if(Auth::check())
-	<iframe src="https://kiwiirc.com/client/irc.freenode.net/?&nick={{ Auth::user()->name }}#laravel" style="border:0; width:100%; height:450px;"></iframe>
+@if(Auth::check() or Session::has('userLazilyOptsOutOfAuthOnChat'))
+	<iframe src="https://kiwiirc.com/client/irc.freenode.net/?&nick={{ Auth::check() ? Auth::user()->name : 'laravelnewbie'}}#laravel" style="border:0; width:100%; height:450px;"></iframe>
 	<a href="http://irclogs.laravel.io" target="_NEW">Search the chat logs</a> to see if your question has already been answered. You can use your own IRC client at Freenode.net in #Laravel.<br>
 	Channel Moderators are: TaylorOtwell, ShawnMcCool, PhillSparks, daylerees, JasonLewis, machuga and JesseOBrien.
 @else
-	In order to use the online chat, you must first login. The signup process is a painless authentication with GitHub. <a href="{{ action('Controllers\AuthController@getLogin') }}">Authenticate with GitHub</a>
+	{{-- look at how much I don't give a crap --}}
+	<?Session::put('userLazilyOptsOutOfAuthOnChat', 1)?>
+	<p>
+		Before you use chat, it'd be best if you logged into Laravel.IO. The signup process is a painless authentication with GitHub, you don't actually have to enter anything. <a href="{{ action('Controllers\AuthController@getLogin') }}">Authenticate with GitHub</a>
+	</p>
+
+	<p>
+		Or, if you can't be arsed to authenticate (just do it) then.. 
+	</p>
 @endif
