551aac20dc057a63c6367892f76acf63357cbc67
Nick Spelt <nickstr@nickstr.nl>
2013-09-22 22:56:14 +0200

    Small updates to design

--- app/views/articles/index.blade.php
+++ app/views/articles/index.blade.php
@@ -1,3 +1,35 @@
-<h1>Articles</h1>
+<section id="sidebar">
+    <div id="categories">
+        <h4>Categories</h4>
+        <ul>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+            <li><a href="#">Security/Auth</a></li>
+        </ul>
+    </div>
+    <div id="new">
+        <h4>Add New</h4>
+        <p>You think you got what it takes to be the very best?</p>
+        <a class="button full" href="{{ action('Controllers\ArticlesController@getCompose') }}">Write an Article</a>
+    </div>
+</section>
 
-<a href="{{ action('Controllers\ArticlesController@getCompose') }}">Write an Article</a>
+<section id="articles">
+<h2>Articles</h2>
+
+    <article>
+        <h2>The Laravel Foundations Project</h2>
+            <ul class="meta">
+                <li><i class="icon-time"></i> 2 Days ago</li>
+                <li><i class="icon-user"></i> Shawn McCool</li>
+                <li><i class="icon-comments"></i> 3 Comments</li>
+            </ul>
+        <p>The goal of this project is to enlist a number of experience Laravel developers who can help write articles about the common questions that come up in the Laravel support community settings (such as IRC and the forums).</p>
+        <a class="more" href="article.html"><span>Read more</span></a>
+    </article>
+</section>
--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,7 +1,38 @@
-Hello!
+<secion class="hero-section">
+    <div class="description">
+        <h1>Articles</h1>
+        <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </div>
+    <div class="posts">
+        @include('home._post')
+    </div>
+</section>
 
-@if(Auth::check())
-    You are logged in as {{ Auth::user()->name }}.
-@else
-    <a href="{{ action('Controllers\AuthController@getLogin') }}">Login with GitHub</a>
-@endif
+<secion class="hero-section dark">
+    <div class="description">
+        <h1>Articles</h1>
+        <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </div>
+    <div class="posts">
+
+    </div>
+</section>
+
+<secion class="hero-section darker">
+    <div class="description">
+        <h1>Articles</h1>
+        <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </div>
+    <div class="posts">
+
+    </div>
+</section>
