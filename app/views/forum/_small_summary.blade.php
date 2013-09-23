09d8854c975b3497c2bc02d815aae400e07220c2
Shawn McCool <shawn@heybigname.com>
2013-09-23 22:02:53 +0200

    add forum thread url to home index

--- app/views/forum/_small_summary.blade.php
+++ app/views/forum/_small_summary.blade.php
@@ -1,5 +1,5 @@
 <div class="post">
-    <h3><a href="{{ $thread->showUrl }}">{{ $thread->title }}</a></h3>
+    <h3><a href="{{ $thread->forumThreadUrl }}">{{ $thread->title }}</a></h3>
     <ul class="meta">
         <li><i class="icon-time"></i> {{ $thread->created_ago }}</li>
         <li><i class="icon-user"></i> {{ $thread->author->name }}</li>
