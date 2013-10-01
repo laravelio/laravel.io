dae16e2c3891f805816aa109811a2030d706dcf8
Justin Seiter <justinseiter@gmail.com>
2013-09-30 21:43:16 -0500

    Add bourbon grid to sidebar and articles.

--- app/views/forum/_thread_summary.blade.php
+++ app/views/forum/_thread_summary.blade.php
@@ -1,4 +1,4 @@
-<div>
+<article>
     <a href="{{ $thread->forumThreadUrl }}">{{ $thread->title }}</a>
     <a href="{{ $thread->author->profileUrl }}">{{ $thread->author->name }}</a>
-</div>
+</article>
