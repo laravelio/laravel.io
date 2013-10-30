c7fc77ff14400cf524f1862190a1b345f20f5121
Nick Spelt <nickstr@nickstr.nl>
2013-10-30 15:39:16 +0100

    auth style updates

--- app/views/auth/signup.blade.php
+++ app/views/auth/signup.blade.php
@@ -1,3 +1,9 @@
-Hi, you'll need an account in order to do whatever you just tried to do. No problem, just authenticate through GitHub and you're done.
+@extends('layouts._one_column')
 
-<a href="{{ action('AuthController@getLogin') }}">Login with GitHub</a>
+@section('content')
+<section class="auth woops">
+    <h1>Woops!</h1>
+    <p>You'll need an account in order to do whatever you just tried to do. No problem, just authenticate through GitHub and you're done.</p>
+    <a class="button full" href="{{ action('AuthController@getLogin') }}">Login with GitHub</a>
+</section>
+@stop
