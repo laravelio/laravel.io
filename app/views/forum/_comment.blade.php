d9483c7903273c2bd99208270b7f05bea7c9db86
Shawn McCool <shawn@heybigname.com>
2013-10-30 16:50:23 +0100

    add clickable profile images

---
+++ app/views/forum/_comment.blade.php
@@ -0,0 +1,23 @@
+<div class="comment" id="comment-{{ $comment->id }}">
+    <div class="user">
+        <a href="{{ $comment->author->profileUrl }}">{{ $comment->author->thumbnail }}</a>
+    </div>
+    <div class="content">
+        @if($comment->id == $thread->id)
+            <h1>{{ $comment->title }}</h1>
+            <div class="tags">Tags:  {{ $comment->tags->getTagList() }}</div>
+            {{ $comment->body }}
+            <ul class="meta">
+                <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
+                <li><i class="icon-user"></i><a href="{{ $comment->author->profileUrl }}"> {{ $comment->author->name }}</a></li>
+            </ul>
+        @else
+            {{ $comment->body }}
+              <ul class="meta">
+                <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
+                <li><i class="icon-user"></i> <a href="{{ $comment->author->profileUrl }}">{{ $comment->author->name }}</a></li>
+                <li><i class="icon-link"></i> <a href="{{ $comment->commentUrl }}">Share</a></li>
+            </ul>
+        @endif
+    </div>
+</div>
--- app/views/forum/thread.blade.php
+++ app/views/forum/thread.blade.php
@@ -9,29 +9,7 @@
 <div class="row forum">
     <div class="small-12 columns comments">
         @foreach($comments as $comment)
-            <div class="comment" id="comment-{{ $comment->id }}">
-                <div class="user">
-                    {{ $comment->author->thumbnail  }}
-                </div>
-                <div class="content">
-                    @if($comment->id == $thread->id)
-                        <h1>{{ $comment->title }}</h1>
-                        <div class="tags">Tags:  {{ $comment->tags->getTagList() }}</div>
-                        {{ $comment->body }}
-                        <ul class="meta">
-                            <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
-                            <li><i class="icon-user"></i><a href="{{ $comment->author->profileUrl }}"> {{ $comment->author->name }}</a></li>
-                        </ul>
-                    @else
-                        {{ $comment->body }}
-                          <ul class="meta">
-                            <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
-                            <li><i class="icon-user"></i> <a href="{{ $comment->author->profileUrl }}">{{ $comment->author->name }}</a></li>
-                            <li><i class="icon-link"></i> <a href="{{ $comment->commentUrl }}">Share</a></li>
-                        </ul>
-                    @endif
-                </div>
-            </div>
+            @include('forum._comment')
         @endforeach
 
     {{ $comments->links() }}
