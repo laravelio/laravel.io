74cac184f9960ee88975eae2bfdecf9c0d45978f
Shawn McCool <shawn@heybigname.com>
2013-10-30 16:43:43 +0100

    together, we can make amazingly awful code

--- app/domain/Lio/Markdown/Markdown.php
+++ app/domain/Lio/Markdown/Markdown.php
@@ -7,11 +7,6 @@ class Markdown extends MarkdownExtra
     public function transform($content)
     {
         $parsedContent = parent::transform($content);
-
-        // $parsedContent = str_replace('<p><code>', '<pre><code>', $parsedContent);
-        // $parsedContent = str_replace('</code></p>', '</code></pre>', $parsedContent);
-
-        // $parsedContent = str_replace('<pre><code>', '<pre class="prettyprint"><code>', $parsedContent);
         $parsedContent = str_replace('<code>', '<code class="prettyprint">', $parsedContent);
 
         return $parsedContent;
