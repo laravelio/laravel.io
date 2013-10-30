f0b438ba952af88410d77c993039b232e93243ae
Shawn McCool <shawn@heybigname.com>
2013-10-30 16:56:14 +0100

    bind icons to links in comments

--- app/views/forum/_comment.blade.php
+++ app/views/forum/_comment.blade.php
@@ -14,9 +14,9 @@
         @else
             {{ $comment->body }}
               <ul class="meta">
-                <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
-                <li><i class="icon-user"></i> <a href="{{ $comment->author->profileUrl }}">{{ $comment->author->name }}</a></li>
-                <li><i class="icon-link"></i> <a href="{{ $comment->commentUrl }}">Share</a></li>
+                <li><i class="icon-time"></i>&nbsp;{{ $comment->created_ago }}</li>
+                <li><i class="icon-user"></i>&nbsp;<a href="{{ $comment->author->profileUrl }}">{{ $comment->author->name }}</a></li>
+                <li><i class="icon-link"></i>&nbsp;<a href="{{ $comment->commentUrl }}">Share</a></li>
             </ul>
         @endif
     </div>
