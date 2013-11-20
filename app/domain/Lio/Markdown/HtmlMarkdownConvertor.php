fd1f028e3d85c3cf10ae759a5a48dcce1da3d83c
Shawn McCool <shawn@heybigname.com>
2013-11-20 17:03:26 +0100

    add markdown convertor class and library dependencies

---
+++ app/domain/Lio/Markdown/HtmlMarkdownConvertor.php
@@ -0,0 +1,27 @@
+<?php namespace Sterling\Markdown;
+
+use HTML_To_Markdown;
+
+class HtmlMarkdownConvertor
+{
+    protected $htmlParser;
+    protected $markdownParser;
+
+    public function __construct()
+    {
+        $this->htmlParser     = new HTML_To_markdown;
+        $this->htmlParser->set_option('header_style', 'atx');
+
+        $this->markdownParser = new \Michelf\MarkdownExtra;
+    }
+
+    public function convertHtmlToMarkdown($html)
+    {
+        return $this->htmlParser->convert($html);
+    }
+
+    public function convertMarkdownToHtml($markdown)
+    {
+        return $this->markdownParser->transform($markdown);
+    }
+}
