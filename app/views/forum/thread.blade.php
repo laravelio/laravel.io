e6b7022393a7cba4a176c6be6fe6d9d01d5de88d
Shawn McCool <shawn@heybigname.com>
2013-12-04 12:39:09 +0100

    tweaks to gist language

--- app/views/forum/thread.blade.php
+++ app/views/forum/thread.blade.php
@@ -37,7 +37,7 @@
                                 {{ $errors->first('body', '<small class="error">:message</small>') }}
 
                                 {{ Form::button('Reply', ['type' => 'submit', 'class' => 'button']) }}
-                                <small>Paste a <a href="https://gist.github.com">Gist</a> URL to embed source. <em>example: https://gist.github.com/username/1234</em></small>
+                                <small>Paste a <a href="https://gist.github.com" target="_NEW">Gist</a> URL to embed source. <em>example: https://gist.github.com/username/1234</em></small>
                             </div>
                         </div>
                     </fieldset>
