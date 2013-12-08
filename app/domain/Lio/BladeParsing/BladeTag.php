09ef29b4a2fca14a713dcb8693aa8d0dae0c5efc
Shawn McCool <shawn@heybigname.com>
2013-12-08 23:11:01 +0100

    add fortrabbit link

--- app/domain/Lio/BladeParsing/BladeTag.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Lio\BladeParsing;
-
-interface BladeTag
-{
-    public function getPattern();
-    public function getMatchCount($view);
-    public function transform($view);
-}
--- app/views/layouts/_footer.blade.php
+++ app/views/layouts/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    Proudly hosted by: <a href="/" class="fortrabbit home"><em>•</em> fortrabbit</a>
+    Proudly hosted by: <a href="http://fortrabbit.com" class="fortrabbit home"><em>•</em> fortrabbit</a>
 </footer>
