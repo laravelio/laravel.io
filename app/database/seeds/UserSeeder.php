31f1660b9b859d3a58fb67a272fb70f04df652c8
Shawn McCool <shawn@heybigname.com>
2013-12-08 16:40:11 +0100

    make tags title case remove test shawn user account

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -13,14 +13,5 @@ class UserSeeder extends Seeder
 
     private function createUsers()
     {
-        User::create([
-            'id'         => 1,
-            'email'      => 'shawn@heybigname.com',
-            'name'       => 'ShawnMcCool',
-            'github_url' => 'https://github.com/ShawnMcCool',
-            'image_url'  => 'https://2.gravatar.com/avatar/c7d7ea7ed7cdf742ebc2c9445b9928c3?d=https%3A%2F%2Fidenticons.github.com%2F5acd34b4fee131776d30ebf26350f99e.png',
-            'created_at' => '2013-09-22 18:01:57',
-            'updated_at' => '2013-09-23 15:25:16',
-        ]);
     }
 }
