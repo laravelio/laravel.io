d020fab4be1ba483c81aaef9b76b410560497976
Shawn McCool <shawn@heybigname.com>
2013-12-14 20:29:30 +0100

    fix bug in article summary partial

--- app/views/articles/_summary.blade.php
+++ app/views/articles/_summary.blade.php
@@ -2,7 +2,7 @@
     <h1><a class="more" href="{{ $article->showUrl }}">{{ $article->title }}</a></h1>
         <ul class="meta">
             <li><i class="icon-time"></i> {{ $article->published_ago }}</li>
-            <li><i class="icon-user"></i> <a href="{{ $comment->author->profileUrl }}">{{ $article->author->name }}</a></li>
+            <li><i class="icon-user"></i> <a href="{{ $article->author->profileUrl }}">{{ $article->author->name }}</a></li>
             <li><i class="icon-comments"></i> {{ $article->comment_count_label }}</li>
         </ul>
     <p>{{ $article->summary }}</p>
