23917415a2437b87e71e34cab6c84f7402b72982
Nick Spelt <nickstr@nickstr.nl>
2013-12-20 17:27:28 +0100

    started styling profile page

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -7,44 +7,23 @@
 @section('content')
 <section class="user-content">
 
-
 @if($threads->count() > 0)
-    <div class="half">
-        <h1>{{ $user->name }}'s Latest Threads</h1>
-        <div class="comments">
-            @foreach($threads as $thread)
-                <div class="comment">
-                    <div class="content">
-                            <h2>{{ $thread->title }}</h2>
-                            {{ $thread->bodySummary }}
-                              <ul class="meta">
-                                <li><i class="icon-time"></i> {{ $thread->created_ago }}</li>
-                                <li><a href="{{ $thread->forumThreadUrl }}"><i class="icon-eye"></i> View Thread</a></li>
-                            </ul>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+    <div class="header">
+        <h1>Threads by {{ $user->name }}</h1>
+    </div>
+    <div class="threads">
+        @each('forum._thread_summary', $threads, 'thread')
     </div>
 @endif
 
+
+
 @if($comments->count())
-    <div class="half">
-        <h1>{{ $user->name }}'s Latest Replies</h1>
-        <div class="comments">
-            @foreach($comments as $comment)
-                <div class="comment">
-                    <div class="content">
-                        <h2>RE: {{ $comment->parent->title }}</h2>
-                        {{ $comment->bodySummary }}
-                          <ul class="meta">
-                            <li><i class="icon-time"></i> {{ $comment->created_ago }}</li>
-                            <li><a href="{{ $comment->commentUrl }}"><i class="icon-eye"></i> View Comment</a></li>
-                        </ul>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+    <div class="header double">
+        <h1>Replies by {{ $user->name }}</h1>
+    </div>
+    <div class="threads">
+        @each('forum._thread_summary', $comments, 'thread')
     </div>
 @endif
 
