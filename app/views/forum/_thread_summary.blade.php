012b53377dee8bc7545d733288e254532921839f
Shawn McCool <shawn@heybigname.com>
2013-12-23 23:52:31 +0100

    swap location of 'updated x ago' and 'posted by' on forum index

--- app/views/forum/_thread_summary.blade.php
+++ app/views/forum/_thread_summary.blade.php
@@ -3,8 +3,8 @@
     <div class="info">
         <h3><a href="{{ $thread->forumThreadUrl }}">{{ $thread->laravel_version ? $thread->laravel_version . ' ' : '' }}{{ $thread->title }}</a></h3>
         <ul class="meta">
+            <li>posted by <a href="{{ $thread->author->profileUrl }}">{{ $thread->author->name }}</a></li>
             <li>updated {{ $thread->updated_ago }}</li>
-            <li>by <a href="{{ $thread->author->profileUrl }}">{{ $thread->author->name }}</a></li>
         </ul>
     </div>
     <div class="comment-count {{ $thread->isNewerThan($last_visited_timestamp) ? 'new' : '' }}">{{ $thread->child_count }}</div>
