0403e363e4465331558ac5c956c1299008dc3955
Shawn McCool <shawn@heybigname.com>
2014-01-31 17:00:37 +0100

    add google analytics

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -11,6 +11,19 @@
   @show
 
   <script src="{{ asset('javascripts/vendor/custom.modernizr.js') }}"></script>
+
+  <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-36064061-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+  </script>
+
 </head>
 <body>
 
@@ -29,10 +42,11 @@
 </div>
 
 <div class="push"></div>
-@include('layouts._footer')
+  @include('layouts._footer')
 
   @section('scripts')
   	<script src="{{ asset('javascripts/vendor/jquery.min.js') }}"></script>
+
   @show
 </body>
 </html>
