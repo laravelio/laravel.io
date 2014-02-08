67e6a713fa402e1612e77807d436fcce3c9579fa
Shawn McCool <shawn@heybigname.com>
2014-02-08 16:27:01 +0100

    fix user admin

---
+++ app/controllers/Admin/AdminUsersController.php
@@ -0,0 +1,49 @@
+<?php
+
+use Lio\Accounts\UserRepository;
+use Lio\Accounts\RoleRepository;
+
+class AdminUsersController extends BaseController
+{
+    private $users;
+    private $roles;
+
+    public function __construct(UserRepository $users, RoleRepository $roles)
+    {
+        $this->users = $users;
+        $this->roles = $roles;
+    }
+
+    public function getIndex()
+    {
+        $users = $this->users->getAllPaginated(30);
+        $this->view('admin.users.index', compact('users'));
+    }
+
+    public function getEdit($userId)
+    {
+        $user  = $this->users->requireById($userId);
+        $roles = $this->roles->getAll();
+
+        $this->view('admin.users.edit', compact('user', 'roles'));
+    }
+
+    public function postEdit($userId)
+    {
+        $user = $this->users->requireById($userId);
+
+        $user->fill(Input::all());
+        if ( ! Input::has('is_banned')) {
+            $user->is_banned = 0;
+        }
+
+        if ( ! $user->isValid()) {
+            return $this->redirectBack(['errors' => $user->getErrors()]);
+        }
+
+        $this->users->save($user);
+        $user->roles = Input::get('roles');
+
+        return $this->redirectAction('AdminUsersController@getIndex', ['success' => 'The user has been saved.']);
+    }
+}
