4827aa914bbac72c323201dd7f865ec394460f2d
Flavio Ferreira <flaviometalvale@gmail.com>
2014-03-05 09:44:35 -0500

    Cleanup unclosed tags and remove some tags

--- app/helpers/HtmlClean.php
+++ app/helpers/HtmlClean.php
@@ -2,7 +2,7 @@
 
 class HtmlClean{
     
-    public function make($html){
+    public static function make($html){
         
         $html  = strip_tags($html,"<b><strong><em><i><p><br><ul><li><ol><br><h1><h2><h3><h4><h5><h6><span><blockquote><code><label><pre><div>");
         $tidy  = new tidy();
