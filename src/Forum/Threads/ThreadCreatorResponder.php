05ac6f8623caaa41c78a70488ea963512c7fe4c0
Shawn McCool <shawn@heybigname.com>
2014-03-09 19:12:12 +0100

    remove a bunch of (now) unused classes

--- src/Forum/Threads/ThreadCreatorResponder.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Lio\Forum\Threads;
-
-interface ThreadCreatorResponder
-{
-    public function threadCreationError($errors);
-    public function threadCreated($thread);
-}
--- src/Forum/Threads/ThreadUpdaterResponder.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Lio\Forum\Threads;
-
-interface ThreadUpdaterResponder
-{
-    public function threadUpdateError($errors);
-    public function threadUpdated($thread);
-}
