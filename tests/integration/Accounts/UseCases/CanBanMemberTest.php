4a28da156e381399210df0efbcbd2ea43c49f888
Shawn McCool <shawn@heybigname.com>
2014-05-18 21:53:07 -0400

    improve tests

--- tests/integration/Accounts/UseCases/CanBanMemberTest.php
+++ tests/integration/Accounts/UseCases/CanBanMemberTest.php
@@ -4,10 +4,5 @@ use App;
 
 class CanBanMemberTest extends \IntTestCase
 {
-    private function getHandler()
-    {
-        return new BanMemberHandler(
-            App::make('Lio\Accounts\MemberRepository'),
-            App::make('Lio\Events\Dispatcher'));
-    }
+
 }
