c9a51ca97a3923d317917b37ccb435794201d162
Shawn McCool <shawn@heybigname.com>
2014-05-30 13:59:11 +0200

    stuff

--- app/Lio/Accounts/User.php
+++ app/Lio/Accounts/User.php
@@ -142,4 +142,19 @@ class User extends Entity implements UserInterface, RemindableInterface
     {
         return $this->forumReplies()->with('thread')->paginate($max);
     }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
