733551b04275279d930572ba0c0968866c869532
Dries Vints <dries.vints@gmail.com>
2014-09-10 12:44:08 +0200

    Drop js for subject field

--- app/views/bin/_editor.blade.php
+++ app/views/bin/_editor.blade.php
@@ -1,13 +1,4 @@
 <div class="editor-container">
     {{ Form::textarea('code', null, ['class' => 'editor mousetrap', 'wrap' => 'off']) }}
+    <input type="text" name="subject" style="visibility:hidden;position:absolute;bottom:0;left:0;">
 </div>
-<div id="subjectDiv" style="position: absolute; bottom: 0;left: 0;">
-    <label for="foo">Leave this field blank</label>
-    <input type="text" name="subject" id="subject">
-</div>
-<script>
-(function () {
-    var e = document.getElementById("subjectDiv");
-    e.parentNode.removeChild(e);
-})();
-</script>
