d1ec1e2291e185878bfc00dbbaafd6d8ccb2ee05
fungku <fungku@gmail.com>
2014-11-02 09:09:52 -0800

    Change reply timer from 5 to 2 seconds

--- app/Lio/Forum/Replies/ReplyForm.php
+++ app/Lio/Forum/Replies/ReplyForm.php
@@ -14,12 +14,12 @@ class ReplyForm extends FormModel
 
         // Conditional validation rule:
         //   - compare processing time to form-creation time
-        //   - if the difference is less than 5 seconds
+        //   - if the difference is less than 2 seconds
         //   - we apply a rule where the max length of the _time input is 0
         //   - and then validation will fail because it is not 0
         Validator::sometimes('_time', 'max:0', function() use ($time)
         {
-            return (strtotime("now") - $time) < 5;
+            return (strtotime("now") - $time) < 2;
         });
     }
 }
