d5b67f43116c7e0ca742674f68f7f9d34ec0e864
fungku <fungku@gmail.com>
2014-11-02 09:02:46 -0800

    Add validation for reply and thread completion time

--- app/Lio/Forum/Replies/ReplyForm.php
+++ app/Lio/Forum/Replies/ReplyForm.php
@@ -7,4 +7,19 @@ class ReplyForm extends FormModel
     protected $validationRules = [
         'body'  => 'required',
     ];
+
+    protected function beforeValidation()
+    {
+        $time = Input::get('_time');
+
+        // Conditional validation rule:
+        //   - compare processing time to form-creation time
+        //   - if the difference is less than 5 seconds
+        //   - we apply a rule where the max length of the _time input is 0
+        //   - and then validation will fail because it is not 0
+        Validator::sometimes('_time', 'max:0', function() use ($time)
+        {
+            return (strtotime("now") - $time) < 5;
+        });
+    }
 }
