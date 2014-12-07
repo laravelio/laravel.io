79741f872aa0de3316c1dcb4fde48d10ee7e01d5
Dries Vints <dries.vints@gmail.com>
2014-12-07 13:46:45 +0100

    Add nav shortcut for admins

--- app/views/layouts/_top_nav.blade.php
+++ app/views/layouts/_top_nav.blade.php
@@ -24,10 +24,16 @@
             <li>
                 <a target="_blank" href="http://forumsarchive.laravel.io/">Old Forum Archive</a>
             </li>
+
+            @if (Auth::check() && Auth::user()->hasRole('manage_users'))
+                <li>
+                    <a href="{{ action('Admin\UsersController@getIndex') }}">Admin</a>
+                </li>
+            @endif
         </ul>
     </nav>
     <ul class="user-navigation">
-        @if(Auth::check())
+        @if (Auth::check())
             <li><a href="{{ url($currentUser->profileUrl) }}">{{ $currentUser->name }}</a></li>
             <li><a class="button" href="{{ action('AuthController@getLogout') }}">Logout</a></li>
         @else
