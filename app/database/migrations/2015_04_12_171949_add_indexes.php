40b5c8d28ee10e4793a197a7749abd0aa1912259
Dries Vints <dries.vints@gmail.com>
2015-04-12 19:28:07 +0200

    Add indexes on tables

---
+++ app/database/migrations/2015_04_12_171949_add_indexes.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forum_threads', function (Blueprint $table) {
+            $table->index('author_id');
+            $table->index('most_recent_reply_id');
+            $table->index('solution_reply_id');
+        });
+
+        Schema::table('forum_replies', function (Blueprint $table) {
+            $table->index('author_id');
+            $table->index('thread_id');
+        });
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->index('author_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('forum_threads', function (Blueprint $table) {
+            $table->dropIndex('author_id');
+            $table->dropIndex('most_recent_reply_id');
+            $table->dropIndex('solution_reply_id');
+        });
+
+        Schema::table('forum_replies', function (Blueprint $table) {
+            $table->dropIndex('author_id');
+            $table->dropIndex('thread_id');
+        });
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropIndex('author_id');
+        });
+    }
+}
