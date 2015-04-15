88aa538cb65225b9b707af5dedbd6c290f90c34b
Dries Vints <dries.vints@gmail.com>
2015-04-15 14:05:07 +0200

    Escape search query

--- app/views/forum/search.blade.php
+++ app/views/forum/search.blade.php
@@ -7,19 +7,19 @@
 @section('content')
     <section class="forum">
         <div class="header">
-            @if($query)
-                <h1>Forum Search results for "{{ $query }}"</h1>
+            @if ($query)
+                <h1>Forum Search results for "{{{ $query }}}"</h1>
             @else
                 <h1>Forum Search</h1>
             @endif
         </div>
 
-        @if($query)
+        @if ($query)
             <div class="threads">
-                @if($results->count() > 0)
+                @if ($results->count() > 0)
                     {{-- Loop over the threads and display the thread summary partial --}}
-                    @foreach($results as $result)
-                        @if($result->parent)
+                    @foreach ($results as $result)
+                        @if ($result->parent)
                             @include('forum.threads._index_summary', ['thread' => $result->parent])
                         @else
                             @include('forum.threads._index_summary', ['thread' => $result])
@@ -28,7 +28,7 @@
                 @else
                     {{-- If no comments are found display a message --}}
                     <div class="empty-state">
-                        <h3>No results found on the forum for "{{ $query }}"</h3>
+                        <h3>No results found on the forum for "{{{ $query }}}"</h3>
                     </div>
                 @endif
             </div>
