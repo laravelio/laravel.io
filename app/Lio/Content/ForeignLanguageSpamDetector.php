6187e8bef8018cfcb963d271baa7b13c48b75ed5
Dries Vints <dries.vints@gmail.com>
2015-05-05 13:28:45 +0200

    Add more characters to foreign language detector

--- app/Lio/Content/ForeignLanguageSpamDetector.php
+++ app/Lio/Content/ForeignLanguageSpamDetector.php
@@ -6,6 +6,6 @@ class ForeignLanguageSpamDetector implements SpamDetector
     /** @inheritdoc */
     public function detectsSpam($value)
     {
-        return (bool) preg_match("/[일안명빠에외전나밤】]+/iu", $value);
+        return (bool) preg_match("/[일안명빠에외전나밤노카지소테크카지주소る테】]+/iu", $value);
     }
 }
