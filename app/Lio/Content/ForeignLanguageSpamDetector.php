57479fa6e710c193378ba6abb6b32813e20ceadd
Dries Vints <dries.vints@gmail.com>
2015-05-11 21:55:48 +0200

    Add more characters to spam detector

--- app/Lio/Content/ForeignLanguageSpamDetector.php
+++ app/Lio/Content/ForeignLanguageSpamDetector.php
@@ -6,6 +6,9 @@ class ForeignLanguageSpamDetector implements SpamDetector
     /** @inheritdoc */
     public function detectsSpam($value)
     {
-        return (bool) preg_match("/[일안명빠에외전나밤노카지소테크카지주소る테】]+/iu", $value);
+        return (bool) preg_match(
+            "/[일안명빠에외하전나밤사이팅토노카ぬ벳인포방어코리아맨강남야구장강배팅배트ミ법스포츠석사배지석사소테크카주소る】ズ]+/iu",
+            $value
+        );
     }
 }
