1af74a61147e755689b12c092a25ee69c45d439b
Dries Vints <dries.vints@gmail.com>
2015-05-31 23:48:14 +0200

    Update foreign language spam detector

--- src/Content/ForeignLanguageSpamDetector.php
+++ src/Content/ForeignLanguageSpamDetector.php
@@ -7,7 +7,7 @@ class ForeignLanguageSpamDetector implements SpamDetector
     public function detectsSpam($value)
     {
         return (bool) preg_match(
-            "/[일안명빠에외하전나밤사이팅토노카ぬ벳인포방어코리아맨강남야구장강배팅배트ミ법스포츠석사배지석사소테크카주소る】ズ]+/iu",
+            "/[일안명빠에외하전나밤사이팅토노카ぬ벳인포방어코리아맨강남야구장강배팅배트ミ법스포츠석사배지석사소테크카주소る】ズअ서현오피अ분당오피अ]+/iu",
             $value
         );
     }
