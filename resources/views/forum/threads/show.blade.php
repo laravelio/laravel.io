46e4adeca6dfc4458361ae38c931bb865ae1b798
Dries Vints <dries.vints@gmail.com>
2015-11-23 09:12:35 +0100

    Hide reply box when not logged in

--- resources/views/forum/threads/show.blade.php
+++ resources/views/forum/threads/show.blade.php
@@ -10,24 +10,26 @@
         <a href="{{ route('threads.delete', $thread->slug()) }}">Delete</a>
     </p>
 
-    <hr>
-
     @if (count($replies = $thread->replies()))
         @foreach ($replies as $reply)
+            <hr>
             <p>@md($reply->body())</p>
             <p>By {{ $reply->author()->name() }} - {{ $reply->createdAt()->diffForHumans() }}</p>
             <p>
                 <a href="{{ route('replies.edit', $thread->id()) }}">Edit</a> |
                 <a href="{{ route('replies.delete', $thread->id()) }}">Delete</a>
             </p>
-            <hr>
         @endforeach
     @endif
 
-    {!! Form::open(['route' => 'replies.store']) !!}
-        {!! Form::textarea('body') !!}
-        {!! Form::hidden('replyable_id', $thread->id()) !!}
-        {!! Form::hidden('replyable_type', 'threads') !!}
-        {!! Form::submit('Reply') !!}
-    {!! Form::close() !!}
+    @if (Auth::check())
+        <hr>
+
+        {!! Form::open(['route' => 'replies.store']) !!}
+            {!! Form::textarea('body') !!}
+            {!! Form::hidden('replyable_id', $thread->id()) !!}
+            {!! Form::hidden('replyable_type', 'threads') !!}
+            {!! Form::submit('Reply') !!}
+        {!! Form::close() !!}
+    @endif
 @stop
