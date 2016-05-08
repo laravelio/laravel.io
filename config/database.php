003bd3dc4d0322d0abf70ea82a93b3c6ac89f4a6
Dries Vints <dries.vints@gmail.com>
2016-05-08 12:42:30 +0200

    Use default Laravel settings for sqlite

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => ':memory:',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
