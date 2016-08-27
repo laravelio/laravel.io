b205090bf9ea880176571d80c361e0186d5b28e5
Dries Vints <dries.vints@gmail.com>
2016-08-27 16:21:28 +0200

    Remove buttons from homescreen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,5 @@
     <div class="jumbotron text-center">
         <h1>Laravel.io</h1>
         <h2>The Laravel Community Portal</h2>
-        <p>
-            <a class="btn btn-info" href="#" role="button">Write an Article</a>
-            <a class="btn btn-info" href="{{ route('threads.create') }}" role="button">Start a Discussion</a>
-        </p>
     </div>
 @stop
