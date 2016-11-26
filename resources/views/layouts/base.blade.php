fb32ae76075f33a11a288be785f3a97b81cb5289
Dries Vints <dries.vints@gmail.com>
2016-11-26 17:45:31 +0100

    Improve footer a bit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,9 +17,10 @@
 
     @yield('body')
 
-    <div class="container text-center">
+    <div id="footer" class="container text-center">
         <hr>
         Copyright &copy; 2013 - {{ date('Y') }} Laravel.io &bull;
+        <a href="https://facebook.com/laravelio"><i class="fa fa-facebook"></i></a>
         <a href="https://twitter.com/laravelio"><i class="fa fa-twitter"></i></a>
         <a href="https://github.com/laravelio"><i class="fa fa-github"></i></a>
     </div>
