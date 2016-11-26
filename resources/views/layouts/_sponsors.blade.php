041bc4736d88809e0e3fe523a671282f53ce639f
Dries Vints <dries.vints@gmail.com>
2016-11-26 18:21:05 +0100

    Add sponsors to footer

---
+++ resources/views/layouts/_sponsors.blade.php
@@ -0,0 +1,36 @@
+<div id="sponsors" class="container">
+    <h2>Sponsors</h2>
+
+    <div class="sponsors-list">
+        <div>
+            <a href="https://www.digitalocean.com/?refcode=7922b1dff899">
+                <img src="{{ asset('images/digitalocean.png') }}">
+            </a>
+        </div>
+        <div>
+            <a href="https://ottomatik.io/?refcode=bPeKyjQV">
+                <img src="{{ asset('images/ottomatik.png') }}">
+            </a>
+        </div>
+        <div>
+            <a href="https://forge.laravel.com/">
+                <img src="{{ asset('images/forge.png') }}">
+            </a>
+        </div>
+        <div>
+            <a href="https://envoyer.io/">
+                <img src="{{ asset('images/envoyer.png') }}">
+            </a>
+        </div>
+        <div>
+            <a href="https://cartalyst.com/?utm_source=laravelio&utm_medium=banner&utm_campaign=laravelio">
+                <img src="{{ asset('images/cartalyst.png') }}">
+            </a>
+        </div>
+        <div>
+            <a href="https://bugsnag.com/?r=xUVct0">
+                <img src="{{ asset('images/bugsnag.png') }}">
+            </a>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,13 +17,7 @@
 
     @yield('body')
 
-    <div id="footer" class="container text-center">
-        <hr>
-        Copyright &copy; 2013 - {{ date('Y') }} Laravel.io &bull;
-        <a href="https://facebook.com/laravelio"><i class="fa fa-facebook"></i></a>
-        <a href="https://twitter.com/laravelio"><i class="fa fa-twitter"></i></a>
-        <a href="https://github.com/laravelio"><i class="fa fa-github"></i></a>
-    </div>
+    @include('layouts._footer')
 
     <script src="{{ elixir('js/app.js') }}"></script>
 </body>
