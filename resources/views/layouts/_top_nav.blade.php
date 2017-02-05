a9856fbbbb90179cb5947b10641068da9a051fcc
Dries Vints <dries.vints@gmail.com>
2017-02-05 16:16:04 +0100

    Change url to pastebin

--- resources/views/layouts/_top_nav.blade.php
+++ resources/views/layouts/_top_nav.blade.php
@@ -16,7 +16,7 @@
                 <a class="{{ Request::is('chat*') ? 'active' : null }}" href="{{ action('ChatController@getIndex') }}">Live Chat</a>
             </li>
             <li>
-                <a href="{{ action('PastesController@getCreate') }}">Pastebin</a>
+                <a href="http://paste.laravel.io">Pastebin</a>
             </li>
             <li>
                 <a href="http://www.laravelpodcast.com">Podcast</a>
