c98044a4f57c737621ab41aa0c84b3c7dd10065c
Dries Vints <dries.vints@gmail.com>
2017-04-10 23:52:44 +0200

    Escape HTML in Markdown rendering

--- app/Markdown/MarkdownServiceProvider.php
+++ app/Markdown/MarkdownServiceProvider.php
@@ -10,7 +10,7 @@ class MarkdownServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(Converter::class, function () {
-            return new LeagueConverter(new CommonMarkConverter());
+            return new LeagueConverter(new CommonMarkConverter(['html_input' => 'escape']));
         });
     }
 }
