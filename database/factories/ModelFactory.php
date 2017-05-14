f834f79c6a7ddb3b05e50bd631b171c0961ad1fa
Dries Vints <dries.vints@gmail.com>
2017-05-14 22:46:24 +0200

    Remove forgotten description param

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -59,6 +59,5 @@ $factory->define(Tag::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->text(15),
         'slug' => $faker->slug,
-        'description' => $faker->text,
     ];
 });
