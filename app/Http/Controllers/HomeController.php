553b157f2b8ca13f9b9cb6d0ba1380b93657f326
Dries Vints <dries.vints@gmail.com>
2017-05-14 19:32:09 +0200

    Remove unused threads

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Thread;
-
 class HomeController extends Controller
 {
     public function show()
     {
-        return view('home', ['threads' => Thread::latest()]);
+        return view('home');
     }
 }
