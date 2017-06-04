b83344cf3336a694ddd5f4551aa86916af02628b
Dries Vints <dries.vints@gmail.com>
2017-06-04 19:02:50 +0200

    Make base controller abstract

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, SendsAlerts, ValidatesRequests;
 }
