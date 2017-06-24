a74c0efd182a5f2e575473010dd37ad42ea9738e
Robin Malfait <malfait.robin@gmail.com>
2017-06-24 12:43:04 +0200

    Add BC link for /bin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redirect;
+
 class HomeController extends Controller
 {
     public function show()
@@ -12,5 +14,10 @@ class HomeController extends Controller
     public function rules()
     {
         return view('rules');
+    }
+
+    public function redirectToPaste()
+    {
+        return Redirect::to('https://paste.laravel.io');
     }
 }
