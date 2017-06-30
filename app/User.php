e998cd61a44b40e27ecf6242f03e1ae483c3c7ee
Tamizh <tamizh@terareach.com>
2017-06-30 19:20:37 +0530

    latest thread query issue with order fixed

--- app/User.php
+++ app/User.php
@@ -154,7 +154,7 @@ class User extends Authenticatable
      */
     public function latestThreads(int $amount = 3)
     {
-        return $this->threadsRelation->take($amount);
+        return $this->threadsRelation()->limit($amount)->get();
     }
 
     public function deleteThreads()
