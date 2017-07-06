91e073b230bcb3679212cca23c9046eedc7ebeaa
Dries Vints <dries.vints@gmail.com>
2017-07-06 20:34:04 +0200

    Update rules

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -7,7 +7,7 @@
     <ul>
         <li>Follow our <a href="https://github.com/laravelio/portal/blob/master/code_of_conduct.md" class="alert-link">Code of Conduct</a></li>
         <li>Not spam</li>
-        <li>Not advertise commercial content or products in any way</li>
+        <li>Not advertise commercial content, products or services in any way. You <i>may</i> offer your services or suggest products when asked for but you cannot start threads for them.</li>
         <li>Not recruit for your own or other companies</li>
     </ul>
     <p>Not following these rules may result in the banning or deletion of your profile and/or content.</p>
