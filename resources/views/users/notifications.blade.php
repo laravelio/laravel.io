e7065275ffbe417d9614876e90df98be29fc42eb
Lucas Leandro <lucasleandro1204@gmail.com>
2017-07-11 01:08:28 -0300

    basics

---
+++ resources/views/users/notifications.blade.php
@@ -0,0 +1,17 @@
+@title('Notifications')
+
+@extends('layouts.default')
+
+@section('content')
+    <h1>{{ $title }}</h1>
+    <hr>
+
+    <div class="row">
+        <div class="col-md-3">
+
+        </div>
+        </div>
+        <div class="col-md-9">
+        </div>
+    </div>
+@endsection
