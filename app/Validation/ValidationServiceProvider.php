07ef7250ab8b67343a4365609855581e2d372e04
Adrian Daras <getpingui@gmail.com>
2017-07-11 10:18:44 +0200

    Hide "Current Password" if linked via GitHub

--- app/Validation/ValidationServiceProvider.php
+++ app/Validation/ValidationServiceProvider.php
@@ -11,6 +11,7 @@ class ValidationServiceProvider extends ServiceProvider
     {
         Validator::extend(DoesNotContainUrlRule::NAME, DoesNotContainUrlRule::class.'@validate');
         Validator::extend(PasscheckRule::NAME, PasscheckRule::class.'@validate');
+        Validator::extend(RequiredHasPassword::NAME, RequiredHasPassword::class.'@validate');
         Validator::extend(SpamRule::NAME, SpamRule::class.'@validate');
     }
 }
