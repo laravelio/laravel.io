ab599ab9f56aba80669819ca42dada9d6b2a9afe
Lucas Leandro <lucasleandro1204@gmail.com>
2017-07-13 03:07:19 -0300

    test

---
+++ tests/Components/Jobs/ToggleThreadSubscriptionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Components\Jobs;
+
+use App\User;
+use Tests\TestCase;
+use App\Models\Thread;
+use App\Jobs\ToggleThreadSubscription;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ToggleThreadSubscriptionTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function we_can_toggle_the_user_thread_subscription()
+    {
+        $user = $this->createUser();
+        $thread = factory(Thread::class)->create();
+
+        $job = new ToggleThreadSubscription($thread, $user);
+
+        $job->handle();
+        $this->assertCount(1, $thread->subscribers()->get());
+
+        $job->handle();
+        $this->assertCount(0, $thread->subscribers()->get());
+    }
+}
