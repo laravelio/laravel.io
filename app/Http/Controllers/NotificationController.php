056ab453e0822020830f5c98e561d16d6460fe62
Lucas Leandro <lucasleandro1204@gmail.com>
2017-07-13 03:08:08 -0300

    return view

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Middleware\Authenticate;
+
 class NotificationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(Authenticate::class);
+    }
+
     public function index()
     {
-        return view('users.notifications');
+        $notifications = auth()->user()->notifications()->get();
+
+        return view('users.notifications', compact('notifications'));
     }
 }
