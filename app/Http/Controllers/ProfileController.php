6034b58e1f8a5640ab689bbf97764b71a48915da
johannes.pichler <johannes.pichler@karriere.at>
2017-08-16 11:17:52 +0200

    add avatar route with name initials

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
+use LasseRafn\InitialAvatarGenerator\InitialAvatar;
 
 class ProfileController extends Controller
 {
     public function show(User $user)
     {
         return view('users.profile', compact('user'));
+    }
+
+    public function avatar(User $user, Request $request)
+    {
+        $avatar = new InitialAvatar();
+
+        $size = $request->get('s', 100);
+
+        return $avatar
+            ->name($user->name())
+            ->background('#149c82')
+            ->size($size)
+            ->generate()
+            ->response('png', 100);
     }
 }
--- app/User.php
+++ app/User.php
@@ -74,8 +74,9 @@ class User extends Authenticatable
     public function gravatarUrl($size = 100): string
     {
         $hash = md5(strtolower(trim($this->email)));
+        $default = urlencode(route('avatar',  ['username' => $this->username(), 's' => $size]));
 
-        return "https://www.gravatar.com/avatar/$hash?s=$size";
+        return "https://www.gravatar.com/avatar/$hash?s=$size&d=$default";
     }
 
     public function intercomHash(): string
