68dde68ba91dbb12db5d1ee050a735e68668c77f
johannes.pichler <johannes.pichler@karriere.at>
2017-08-29 15:10:44 +0200

    add banned_at column
    migrate is_banned field with updated_at value to banned_at column
    fixes #265

---
+++ database/migrations/2017_08_29_123258_add_banned_at_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBannedAtColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dateTime('banned_at')->nullable();
+        });
+
+        User::where('is_banned', 1)->each(function($user) {
+            $user->banned_at = $user->updated_at;
+            $user->save();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_banned');
+        });
+    }
+}
