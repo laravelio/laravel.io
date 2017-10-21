ce90093ae647c56e0c0f25f2b46fc0fd1376a490
Justin Reasoner <knoxxbox@gmail.com>
2017-10-20 22:26:21 -0400

    dx: enable npm run hot during development

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,8 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/build/js')
-    .sass('resources/assets/sass/app.scss', 'public/build/css')
-    .version();
+    .sass('resources/assets/sass/app.scss', 'public/build/css');
+
+if (mix.config.inProduction) {
+    mix.version();
+}
