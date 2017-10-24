31812a34a9aaeaf7c3e02e34223377bf3d317376
gjrdiesel <knoxxbox@gmail.com>
2017-10-24 08:15:17 -0400

    dress up embedder with es6 (tiny bit quicker)

--- resources/assets/js/embed-twitter.js
+++ resources/assets/js/embed-twitter.js
@@ -1,15 +1,16 @@
 $.fn.twembed = function () {
 
+    const regex = /((http(s|):\/\/)|)twitter.com\/\w*\/status\/\d*/g;
+
     function changeMatchForEmbed(link, content) {
         content.innerHTML = content.innerHTML.replace(
             link,
-            '<blockquote class="twitter-tweet" data-lang="en"><a href="' + link + '">' + link + '</a></blockquote>'
+            `<blockquote class="twitter-tweet" data-lang="en"><a href="${link}">${link}</a></blockquote>`
         )
     }
 
     function regexForMatch(id, content) {
-        var regex = /((http(s|):\/\/)|)twitter.com\/\w*\/status\/\d*/g;
-        var match = regex.exec($(content).text());
+        const match = regex.exec($(content).text());
         if (match && match[0]) {
             changeMatchForEmbed(match[0], content);
         }
