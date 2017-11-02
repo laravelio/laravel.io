fe924e8ae088e33ba5fc8b2f68c1d443e8cca32c
Dries Vints <dries.vints@gmail.com>
2017-11-02 21:56:52 +0100

    Add bugsnag aliases in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Models\Thread;
+use Psr\Log\LoggerInterface;
+use Illuminate\Contracts\Logging\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
@@ -24,5 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function bootMacros()
     {
         require base_path('resources/macros/blade.php');
+    }
+
+    public function register()
+    {
+        $this->app->alias('bugsnag.multi', Log::class);
+        $this->app->alias('bugsnag.multi', LoggerInterface::class);
     }
 }
