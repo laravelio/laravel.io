7bef39ca5f929e790d71b2a1e8979726f6e8df77
Dries Vints <dries.vints@gmail.com>
2017-12-10 11:23:48 +0100

    Don't create models up front

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -4,10 +4,10 @@ use App\User;
 use App\Models\Thread;
 use App\Models\Subscription;
 
-$factory->define(Subscription::class, function (Faker\Generator $faker) {
+$factory->define(Subscription::class, function (Faker\Generator $faker, array $attributes = []) {
     return [
-        'user_id' => factory(User::class)->create()->id(),
-        'subscriptionable_id' => factory(Thread::class)->create()->id(),
+        'user_id' => $attributes['user_id'] ?? factory(User::class)->create()->id(),
+        'subscriptionable_id' => $attributes['subscriptionable_id'] ?? factory(Thread::class)->create()->id(),
         'subscriptionable_type' => Thread::TABLE,
     ];
 });
