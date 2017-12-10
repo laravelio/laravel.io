1ca8aa7da3e2427a7398c54dceb5dfa2daad9c6f
Dries Vints <dries.vints@gmail.com>
2017-12-10 11:20:39 +0100

    Improve factories
    This solves the problem where a model in a factory was created up front. This way the given attribute will be used instead and prevents the creation of countless useless records.

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -4,11 +4,11 @@ use App\User;
 use App\Models\Reply;
 use App\Models\Thread;
 
-$factory->define(Reply::class, function (Faker\Generator $faker) {
+$factory->define(Reply::class, function (Faker\Generator $faker, array $attributes = []) {
     return [
         'body' => $faker->text(),
-        'author_id' => factory(User::class)->create()->id(),
-        'replyable_id' => factory(Thread::class)->create()->id(),
+        'author_id' => $attributes['author_id'] ?? factory(User::class)->create()->id(),
+        'replyable_id' =>  $attributes['replyable_id'] ?? factory(Thread::class)->create()->id(),
         'replyable_type' => Thread::TABLE,
         'ip' => $faker->ipv4,
     ];
--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -3,12 +3,12 @@
 use App\User;
 use App\Models\Thread;
 
-$factory->define(Thread::class, function (Faker\Generator $faker) {
+$factory->define(Thread::class, function (Faker\Generator $faker, array $attributes = []) {
     return [
         'subject' => $faker->text(20),
         'body' => $faker->text,
         'slug' => $faker->slug,
-        'author_id' => factory(User::class)->create()->id(),
+        'author_id' => $attributes['author_id'] ?? factory(User::class)->create()->id(),
         'ip' => $faker->ipv4,
     ];
 });
