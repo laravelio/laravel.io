89ad26af5844faa68dacfc404bc7cc2c195747c7
Raf <7909000@gmail.com>
2018-01-28 01:22:39 +0500

    return blank avatar by default
    for the time as the domain is not fully working over DNS

--- app/User.php
+++ app/User.php
@@ -90,7 +90,8 @@ class User extends Authenticatable
     public function gravatarUrl($size = 100): string
     {
         $hash = md5(strtolower(trim($this->email)));
-        $default = urlencode(route('avatar', ['username' => $this->username()]));
+//        $default = urlencode(route('avatar', ['username' => $this->username()]));
+        $default='blank';
 
         return "https://www.gravatar.com/avatar/$hash?s=$size&d=$default";
     }
