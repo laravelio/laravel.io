e2a41cea27da4bd9376292c4a8440acdbb35b086
Dries Vints <dries.vints@gmail.com>
2018-02-02 21:55:31 +0100

    Implement Horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Thread;
 use Psr\Log\LoggerInterface;
+use Horizon;
 use Illuminate\Contracts\Logging\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->bootEloquentMorphs();
         $this->bootMacros();
+        $this->bootHorizon();
     }
 
     private function bootEloquentMorphs()
@@ -26,6 +28,17 @@ class AppServiceProvider extends ServiceProvider
     public function bootMacros()
     {
         require base_path('resources/macros/blade.php');
+    }
+
+    public function bootHorizon()
+    {
+        if ($horizonEmail = config('lio.horizon_email')) {
+            Horizon::routeMailNotificationsTo($horizonEmail);
+        }
+
+        Horizon::auth(function ($request) use ($horizonEmail) {
+            return auth()->check() && auth()->user()->emailAddress() === $horizonEmail;
+        });
     }
 
     public function register()
