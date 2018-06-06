5e73f29351a392c5e044b8d270f830244a64065d
Dries Vints <dries.vints@gmail.com>
2018-06-06 20:56:25 +0200

    Implement Spatie backups (#373)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
