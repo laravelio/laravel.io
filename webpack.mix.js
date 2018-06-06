f9ce24b3447a13156eb9914546fca9a06e1552ed
Dries Vints <dries.vints@gmail.com>
2018-06-06 22:44:34 +0200

    Always bust asset cache

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,5 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/build/js')
-    .sass('resources/assets/sass/app.scss', 'public/build/css');
-
-if (mix.config.inProduction) {
+    .sass('resources/assets/sass/app.scss', 'public/build/css')
     mix.version();
-}
