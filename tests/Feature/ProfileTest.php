fbde452dbda57426d688972e64317899cbdfddfd
Paul A <paul.androshchuk@gmail.com>
2018-10-04 11:06:47 +0300

    Delete profile tests. #361

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Reply;
+use App\Models\Thread;
+use Illuminate\Support\Facades\Bus;
 use Tests\BrowserKitTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -16,5 +19,29 @@ class ProfileTest extends BrowserKitTestCase
 
         $this->visit('/user/johndoe')
             ->see('John Doe');
+    }
+
+    /** @test */
+    public function anyone_can_delete_their_profile()
+    {
+        $user = $this->login();
+
+        $thread1 = factory(Thread::class)->create(['author_id' => $user->getKey()]);
+        $thread2= factory(Thread::class)->create(['author_id' => $user->getKey()]);
+
+        factory(Reply::class)->create(['author_id' => $user->getKey(), 'replyable_id' => $thread1->getKey()]);
+        factory(Reply::class)->create(['author_id' => $user->getKey(), 'replyable_id' => $thread1->getKey()]);
+        factory(Reply::class)->create(['author_id' => $user->getKey(), 'replyable_id' => $thread2->getKey()]);
+
+        $this->assertEquals(2, $user->countThreads());
+        $this->assertEquals(3, $user->countReplies());
+
+        $this->delete(route('profile.destroy'))
+            ->assertRedirectedToRoute('login');
+
+        $this->assertEquals(0, $user->countThreads());
+        $this->assertEquals(0, $user->countReplies());
+
+        $this->assertFalse($user->exists());
     }
 }
