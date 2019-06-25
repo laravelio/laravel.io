ad491fab93f1de2142779befecceb346fa1aeaf4
Joe Dixon <hello@joedixon.co.uk>
2019-06-25 20:18:53 +0100

    remove dependencies

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,8 +9,6 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
-require('bootstrap-sass');
-
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
