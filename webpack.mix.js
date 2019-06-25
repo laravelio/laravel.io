a338b79afac84eed3467f2c717a5583bf6e5affc
Joe Dixon <hello@joedixon.co.uk>
2019-06-25 20:24:14 +0100

    bootstrap tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
     .version();
