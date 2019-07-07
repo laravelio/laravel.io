f6ca2d5958a097cad86ef9761f0723040ffb1c7b
Joe Dixon <hello@joedixon.co.uk>
2019-07-07 16:46:45 +0100

    add ReplyInput component

---
+++ resources/js/components/ReplyInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <textarea class="editor" name="body" placeholder="Write your reply..."></textarea>
+</template>
+
+<script>
+    import SimpleMDE from 'simplemde';
+    import 'simplemde/dist/simplemde.min.css';
+    export default {
+        mounted() {
+            new SimpleMDE({ 
+                showIcons: ["code"],
+                hideIcons: ['preview', 'side-by-side', 'guide'],
+                element: this.$el.querySelectorAll('editor')[0],
+                status: false
+            });
+        }
+    }
+</script>
