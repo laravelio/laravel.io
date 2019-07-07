411991008621a72fa45e9ac1278d3f898848fd76
Joe Dixon <hello@joedixon.co.uk>
2019-07-07 16:44:17 +0100

    replace jQuery with Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,40 +7,89 @@
 require('./bootstrap');
 
 /**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+const files = require.context('./', true, /\.vue$/i);
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const app = new Vue({
+    el: '#app',
+    data: {
+        activeModal: null
+    },
+    methods: {
+        /**
+         * Add a class to an HTML element
+         */
+        addClass(element, className) {
+            element.classList.add(className);
+        },
+        /**
+         * Remove a class from an HTML element
+         */
+        removeClass(element, className) {
+            element.classList.remove(className);
+        },
+        addDocumentListener() {
+            // when clicking anywhere on the page, remove all subnavs
+            // this will not be fired if a dropdown item is clicked as we stop the propogation of the event 
+            document.addEventListener('click', (e) => {
+                Array.from(document.getElementsByClassName('subnav')).forEach(subnav => {
+                    this.removeClass(subnav, 'active');
+                });
+            });
+        },
+        addNavListeners() {
+            // get all the dropdown elements on the page
+            const dropdowns = document.getElementsByClassName('dropdown');
+            // turn the html collection into an array and loop
+            Array.from(dropdowns).forEach(dropdown => {
+                    dropdown.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    // hide any currently active subnavs
+                    Array.from(document.getElementsByClassName('subnav')).forEach(subnav => {
+                        this.removeClass(subnav, 'active');
+                    });
+                    // activer the subnav of the selected dropdown
+                    this.addClass(dropdown.nextElementSibling, 'active');
+                });
+            });
+        },
+        addSidebarListeners() {
+            // open the sidebar
+            document.getElementById('sidebar-open')
+                .addEventListener('click', (e) => {
+                    document.getElementById('sidebar-open').style.display = 'none';
+                    document.getElementById('sidebar-close').style.display = 'block';
+                    Array.from(document.getElementsByClassName('nav')).forEach(nav => {
+                        nav.classList.add('active');
+                    });
+                });
 
-$('select.selectize').selectize({ maxItems: 3 });
-$('textarea.wysiwyg').markdown({ iconlibrary: 'fa' });
-
-$('.dropdown').on('click', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const activate = $(this).next().hasClass('active') ? false : true;
-    $('.subnav').removeClass('active');
-    if (activate) {
-        $(this).next().addClass('active');
+            // close the sidebar
+            document.getElementById('sidebar-close')
+                .addEventListener('click', (e) => {
+                    document.getElementById('sidebar-open').style.display = 'block';
+                    document.getElementById('sidebar-close').style.display = 'none';
+                    Array.from(document.getElementsByClassName('nav')).forEach(nav => {
+                        removeClass(nav, 'active');
+                    });
+                });
+        }
+    },
+    mounted() {
+        this.addDocumentListener();
+        this.addNavListeners();
+        this.addSidebarListeners();
     }
 });
-
-$(document).on('click', function (e) {
-    $('.subnav').removeClass('active');
-});
-
-$('#sidebar-open').on('click', function (e) {
-    $('#sidebar-open').hide();
-    $('#sidebar-close').show();
-    $('.nav').addClass('active');
-});
-
-$('#sidebar-close').on('click', function (e) {
-    $('#sidebar-open').show();
-    $('#sidebar-close').hide();
-    $('.nav').removeClass('active');
-});
-
-// const app = new Vue({
-//     el: '#app'
-// });
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,59 +2,9 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-window.$ = window.jQuery = require('jquery');
-
-/**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
  * and simple, leaving you to focus on building your next great project.
  */
 
 window.Vue = require('vue');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
-
-/**
- * 3rd Party Libraries.
- */
-require('vue-resource');
-require('selectize');
-require('to-markdown');
-require('bootstrap-markdown/js/bootstrap-markdown');
-require('prismjs');
-require('prismjs/components/prism-json');
-require('prismjs/components/prism-css');
-require('prismjs/components/prism-javascript');
-require('prismjs/components/prism-less');
-require('prismjs/components/prism-sass');
-require('prismjs/components/prism-php');
-window.markdown = require('markdown').markdown;
