77616fa7bd4eb70e0ea2231fa44dfeb78bb968d5
Joe Dixon <hello@joedixon.co.uk>
2019-07-07 16:45:58 +0100

    add modal component

---
+++ resources/js/components/FormModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="modal" v-if="show">
+        <slot/>
+    </div> 
+</template>
+
+<script>
+    export default {
+        props: ['identifier', 'activeModal'],
+        computed: {
+            show() {
+                return this.identifier === this.activeModal
+            }
+        }
+    }
+</script>
--- resources/views/_partials/_update_modal.blade.php
+++ resources/views/_partials/_update_modal.blade.php
@@ -1,19 +1,21 @@
-<div class="modal fade" id="{{ $id }}" tabindex="-1" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            {{ Form::open(['route' => $route, 'method' => 'PUT']) }}
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">{{ $title }}</h4>
-                </div>
-                <div class="modal-body">
-                    {!! $body !!}
+<form-modal identifier="{{ $identifier }}" :active-modal="activeModal">
+    <div class="modal-content" v-cloak>
+        {{ Form::open(['route' => $route, 'method' => 'PUT']) }}
+            <div class="flex flex-col justify-between h-full">
+                <div class="overflow-auto">
+                    <div class="modal-header">
+                        <button type="button" class="close" aria-hidden="true" @click="activeModal = null">&times;</button>
+                        <h4 class="modal-title">{{ $title }}</h4>
+                    </div>
+                    <div class="modal-body">
+                        {!! $body !!}
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    {{ Form::submit($submit ?? $title, ['class' => 'btn btn-warning']) }}
+                    <button type="button" class="text-gray-600 mr-4" @click="activeModal = null">Cancel</button>
+                    {{ Form::submit($submit ?? $title, ['class' => 'button button-primary']) }}
                 </div>
-            {{ Form::close() }}
-        </div>
+            </div>
+        {{ Form::close() }}
     </div>
-</div>
+</form-modal>
