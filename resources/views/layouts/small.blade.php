fe505db819f319e28519f80c7e9365cba92f504f
Joe Dixon <hello@joedixon.co.uk>
2019-07-08 20:41:08 +0100

    fix mobile spacing

--- resources/views/layouts/small.blade.php
+++ resources/views/layouts/small.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base', ['disableAds' => $disableAds ?? true])
 
 @section('body')
-    <div class="flex items-center justify-center my-20">
+    <div class="flex items-center justify-center my-8 md:my-20">
         <div class="w-full md:w-1/2 lg:w-1/3">
             <h1 class="text-4xl text-gray-700 text-center mb-2 font-bold">{{ $title }}</h1>
             <div class="p-8 md:border-2 md:rounded md:bg-gray-100">
