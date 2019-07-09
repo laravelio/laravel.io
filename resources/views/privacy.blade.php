19138203cc3aa2757325f13773d1d8f8748ec394
Joe Dixon <hello@joedixon.co.uk>
2019-07-09 08:28:19 +0100

    style legal pages

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -3,5 +3,7 @@
 @extends('layouts.default')
 
 @section('content')
-    <a href="//www.iubenda.com/privacy-policy/14910149" class="iubenda-white iub-body-embed iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+    <div class="legal-container">
+        <a href="//www.iubenda.com/privacy-policy/14910149" class="iubenda-white iub-body-embed iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+    </div>
 @endsection
--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -3,14 +3,16 @@
 @extends('layouts.small', ['disableAds' => false])
 
 @section('small-content')
-    <p>By posting to the forum and replying to threads you agree to:</p>
-    <ul>
-        <li>Follow our <a href="https://github.com/laravelio/portal/blob/master/code_of_conduct.md" class="alert-link">Code of Conduct</a></li>
-        <li>Not spam</li>
-        <li>Not advertise commercial content, products or services in any way. You <i>may</i> offer your services or suggest products when asked for but you cannot start threads for them.</li>
-        <li>Not recruit for your own or other companies</li>
-    </ul>
-    <p>Not following these rules may result in the banning or deletion of your profile and/or content.</p>
-    <p>Moderators and admins may remove or modify your content at any time they seem necessary without notice.</p>
-    <p>These rules may be changed at any given moment without prior notice.</p>
+    <div class="flex-col rules">
+        <p>By posting to the forum and replying to threads you agree to:</p>
+        <ul class="list-disc list-inside">
+            <li>Follow our <a href="https://github.com/laravelio/portal/blob/master/CODE_OF_CONDUCT.md" class="text-green-dark">Code of Conduct</a></li>
+            <li>Not spam</li>
+            <li>Not advertise commercial content, products or services in any way. You <i>may</i> offer your services or suggest products when asked for but you cannot start threads for them.</li>
+            <li>Not recruit for your own or other companies</li>
+        </ul>
+        <p>Not following these rules may result in the banning or deletion of your profile and/or content.</p>
+        <p>Moderators and admins may remove or modify your content at any time they seem necessary without notice.</p>
+        <p>These rules may be changed at any given moment without prior notice.</p>
+    </div>
 @endsection
