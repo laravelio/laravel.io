19d9d5c71769799e8d7094a1c9af0c78bc4c0452
Joe Dixon <hello@joedixon.co.uk>
2019-07-09 21:43:28 +0100

    style thread create page

--- resources/views/forum/threads/_form.blade.php
+++ resources/views/forum/threads/_form.blade.php
@@ -2,23 +2,31 @@
     @formGroup('subject')
         {!! Form::label('subject') !!}
         {!! Form::text('subject', isset($thread) ? $thread->subject() : null, ['class' => 'form-control', 'required', 'maxlength' => '60']) !!}
-        <span class="help-block">Maximum 60 characters.</span>
+        <span class="text-gray-600 text-sm">Maximum 60 characters.</span>
         @error('subject')
     @endFormGroup
 
     @formGroup('body')
         {!! Form::label('body') !!}
-        {!! Form::textarea('body', isset($thread) ? $thread->body() : null, ['class' => 'form-control wysiwyg', 'required']) !!}
+        {!! Form::textarea('body', isset($thread) ? $thread->body() : null, ['class' => 'editor']) !!}
         @error('body')
     @endFormGroup
 
     @formGroup('tags')
         {!! Form::label('tags') !!}
-        {!! Form::select('tags[]', $tags->pluck('name', 'id'), isset($thread) ? $thread->tags()->pluck('id')->toArray() : [], ['class' => 'form-control selectize', 'multiple']) !!}
-        <span class="help-block">You can choose up to 3 tags.</span>
+        <multi-select 
+e           :options="{{ $tags }}" 
+            :max="3"
+            placeholder="Choose up to 3 tags" 
+            label="name" 
+            track-by="name"
+            name="tags[]">
+        </multi-select>
         @error('tags')
     @endFormGroup
 
-    {!! Form::submit(isset($thread) ? 'Update Thread' : 'Create Thread', ['class' => 'btn btn-primary btn-block']) !!}
-    <a href="{{ isset($thread) ? route('thread', $thread->slug()) : route('forum') }}" class="btn btn-default btn-block">Cancel</a>
+    <div class="flex justify-end items-center">
+        <a href="{{ isset($thread) ? route('thread', $thread->slug()) : route('forum') }}" class="text-green-darker mr-2">Cancel</a>
+        {!! Form::submit(isset($thread) ? 'Update Thread' : 'Create Thread', ['class' => 'button']) !!}
+    </div>
 {!! Form::close() !!}
--- resources/views/forum/threads/create.blade.php
+++ resources/views/forum/threads/create.blade.php
@@ -3,16 +3,27 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>{{ $title }}</h1>
-    <hr>
-
-    <div class="alert alert-info">
-        <p>
-            Please try to search for your question first using
-            <a href="{{ route('forum') }}" class="alert-link">the search box</a> and make sure you've read our
-            <a href="{{ route('rules') }}" class="alert-link">Forum Rules</a> before creating a thread.
-        </p>
+    <div class="bg-white border-b">
+        <div class="container mx-auto flex justify-between items-center px-4">
+            <h1 class="text-xl py-4 text-gray-900">{{ $title }}</h1>
+            
+            {{ Form::open(['route' => 'forum', 'method' => 'GET']) }}
+                {{ Form::text('search', $search ?? null, ['class' => 'rounded border-2 border-gray-300 py-1 px-3 focus:outline-none focus:border-blue-900', 'placeholder' => 'Search for threads...']) }}
+            {{ Form::close() }}
+        </div>
     </div>
 
-    @include('forum.threads._form', ['route' => 'threads.store'])
+    <div class="container mx-auto p-4 flex justify-center">
+        <div class="w-full md:w-2/3 xl:w-1/2">
+            <div class="bg-gray-400 rounded p-2 mb-4 text-gray-700">
+                <p>
+                    Please try to search for your question first using
+                    <a href="{{ route('forum') }}" class="text-green-darker">the search box</a> and make sure you've read our
+                    <a href="{{ route('rules') }}" class="text-green-darker">Forum Rules</a> before creating a thread.
+                </p>
+            </div>
+
+            @include('forum.threads._form', ['route' => 'threads.store'])
+        </div>
+    </div>
 @endsection
