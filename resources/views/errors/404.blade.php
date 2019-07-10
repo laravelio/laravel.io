775ef82666fbba865f45c1d78b6242123bc3fc33
Joe Dixon <hello@joedixon.co.uk>
2019-07-10 21:18:00 +0100

    style error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,8 +3,8 @@
 @extends('layouts.base', ['disableAds' => true])
 
 @section('body')
-    <div class="jumbotron text-center">
-        <h1>{{ $title }}</h1>
-        <p>The page you requested cannot be found.</p>
+    <div class="my-20 text-center text-gray-800">
+        <h1 class="text-5xl">{{ $title }}</h1>
+        <p class="text-lg">The page you requested cannot be found.</p>
     </div>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,11 +3,11 @@
 @extends('layouts.base', ['disableAds' => true])
 
 @section('body')
-    <div class="jumbotron text-center">
-        <h1>{{ $title }}</h1>
-        <p>
+    <div class="my-20 text-center text-gray-800">
+        <h1 class="text-5xl">{{ $title }}</h1>
+        <p class="text-lg">
             We've been notified and will try to fix the problem as soon as possible.<br>
-            Please <a href="https://github.com/laravelio/portal/issues/new">open a Github issue</a> if the problem persists.
+            Please <a href="https://github.com/laravelio/portal/issues/new" class="text-green-darker">open a Github issue</a> if the problem persists.
         </p>
     </div>
 @endsection
