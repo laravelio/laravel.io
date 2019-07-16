27217532b957d62424bfeb9cf9c34c2f6eb20b70
Joe Dixon <hello@joedixon.co.uk>
2019-07-16 20:20:03 +0100

    gather data for homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Cache;
+use App\User;
+use App\Models\Thread;
+
 class HomeController extends Controller
 {
     public function show()
     {
-        return view('home');
+        $totalUsers = Cache::remember('totalUsers', now()->addDay(), function () {
+            return number_format(User::count());
+        });
+
+        $totalThreads = Cache::remember('totalThreads', now()->addDay(), function () {
+            return number_format(Thread::count());
+        });
+
+        $resolutionTime = Cache::remember('resolutionTime', now()->addDay(), function () {
+            return number_format(Thread::resolutionTime());
+        });
+
+        $latestThreads = Thread::feedQuery()
+            ->whereNull('solution_reply_id')
+            ->limit(3)
+            ->get();
+
+        return view('home', [
+            'totalUsers' => $totalUsers,
+            'totalThreads' => $totalThreads,
+            'resolutionTime' => $resolutionTime,
+            'latestThreads' => $latestThreads,    
+        ]);
     }
 
     public function rules()
