002f41276769a90d1fd805577b43250a6cc9e990
Joe Dixon <hello@joedixon.co.uk>
2019-07-16 20:25:01 +0100

    update homepage

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,17 +1,18 @@
 
 <div class="container mx-auto px-4">
     @include('layouts._ads._footer')
-    @include('layouts._sponsors')
-
-    <footer id="footer">
-        Copyright &copy; 2013 - {{ date('Y') }} Laravel.io
-        <span>&bull;</span>
-        <a href="{{ route('terms') }}">Terms</a>
-        <span>&bull;</span>
-        <a href="{{ route('privacy') }}">Privacy</a>
-        <span>&bull;</span>
-        <a href="https://github.com/laravelio"><i class="fa fa-github"></i></a>
-        <a href="https://twitter.com/laravelio"><i class="fa fa-twitter"></i></a>
-        <a href="https://medium.com/laravelio"><i class="fa fa-medium"></i></a>
-    </footer>
 </div>
+
+@include('layouts._sponsors')
+
+<footer id="footer">
+    Copyright &copy; 2013 - {{ date('Y') }} Laravel.io
+    <span>&bull;</span>
+    <a href="{{ route('terms') }}">Terms</a>
+    <span>&bull;</span>
+    <a href="{{ route('privacy') }}">Privacy</a>
+    <span>&bull;</span>
+    <a href="https://github.com/laravelio"><i class="fa fa-github"></i></a>
+    <a href="https://twitter.com/laravelio"><i class="fa fa-twitter"></i></a>
+    <a href="https://medium.com/laravelio"><i class="fa fa-medium"></i></a>
+</footer>
