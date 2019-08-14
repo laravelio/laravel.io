c0320ee568a34f6449e44804e137f8b5f3be89b4
Joe Dixon <hello@joedixon.co.uk>
2019-08-14 21:40:16 +0100

    format BSA ads

--- resources/views/forum/threads/show.blade.php
+++ resources/views/forum/threads/show.blade.php
@@ -18,7 +18,7 @@
                         </reply>
                     </div>
 
-                    <div class="flex flex-col md:flex-row md:items-center text-sm pt-5 border-t mt-4">
+                    <div class="flex flex-col md:flex-row md:items-center text-sm pt-5 border-t my-4">
                         <div class="flex mb-4 md:mb-0">
                             @include('forum.threads.info.avatar', ['user' => $thread->author()])
 
