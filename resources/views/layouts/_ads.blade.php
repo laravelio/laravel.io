e6a41a523665372a4468ab0e3df4b7df6a91acdf
Dries Vints <dries.vints@gmail.com>
2019-08-19 16:33:00 +0200

    Format

--- resources/views/layouts/_ads.blade.php
+++ resources/views/layouts/_ads.blade.php
@@ -1,27 +1,27 @@
 @unless ($disableAds ?? false)
     @if ($adSenseClient = config('services.google.ad_sense.client'))
-    <div id="sidebar-ad" class="ad hidden">
-        <ins class="adsbygoogle block"
-            data-ad-client="{{ $adSenseClient }}"
-            data-ad-slot="{{ config('services.google.ad_sense.unit_forum_sidebar') }}"
-            data-ad-format="rectangle">
-        </ins>
-    </div>
-    <div id="footer-ad" class="hidden">
-        <ins class="adsbygoogle block"
-            style="max-width:728px; max-height:90px"
-            data-ad-client="{{ $adSenseClient }}"
-            data-ad-slot="{{ config('services.google.ad_sense.unit_footer') }}"
-            data-ad-format="auto">
-        </ins>
-    </div>
+        <div id="sidebar-ad" class="ad hidden">
+            <ins class="adsbygoogle block"
+                data-ad-client="{{ $adSenseClient }}"
+                data-ad-slot="{{ config('services.google.ad_sense.unit_forum_sidebar') }}"
+                data-ad-format="rectangle">
+            </ins>
+        </div>
+        <div id="footer-ad" class="hidden">
+            <ins class="adsbygoogle block"
+                style="max-width:728px; max-height:90px"
+                data-ad-client="{{ $adSenseClient }}"
+                data-ad-slot="{{ config('services.google.ad_sense.unit_footer') }}"
+                data-ad-format="auto">
+            </ins>
+        </div>
     @endif
 
     @if ($bsaCpcCode = config('services.bsa.cpc_code'))
         <script src="//m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
         <script>
-            (function(){
-                if(typeof _bsa !== 'undefined' && _bsa) {
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
                     _bsa.init('default', '{{ $bsaCpcCode }}', 'placement:laravelio', {
                         target: '.bsa-cpc',
                         align: 'horizontal',
