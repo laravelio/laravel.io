3b5fda819adad4ce99cff67e905d214c610a448a
Joe Dixon <hello@joedixon.co.uk>
2019-08-20 20:42:33 +0100

    style adsense

--- resources/views/layouts/_ads.blade.php
+++ resources/views/layouts/_ads.blade.php
@@ -1,20 +1,19 @@
 @unless ($disableAds ?? false)
     @if ($adSenseClient = config('services.google.ad_sense.client'))
-        <div id="sidebar-ad" class="ad hidden">
-            <ins class="adsbygoogle block"
-                data-ad-client="{{ $adSenseClient }}"
-                data-ad-slot="{{ config('services.google.ad_sense.unit_forum_sidebar') }}"
-                data-ad-format="rectangle">
-            </ins>
-        </div>
-        <div id="footer-ad" class="hidden">
-            <ins class="adsbygoogle block"
-                style="max-width:728px; max-height:90px"
-                data-ad-client="{{ $adSenseClient }}"
-                data-ad-slot="{{ config('services.google.ad_sense.unit_footer') }}"
-                data-ad-format="auto">
-            </ins>
-        </div>
+    <div id="sidebar-ad" class="ad hidden">
+        <ins class="adsbygoogle sidebar-ad"
+            data-ad-client="{{ $adSenseClient }}"
+            data-ad-slot="{{ config('services.google.ad_sense.unit_forum_sidebar') }}"
+            data-ad-format="rectangle">
+        </ins>
+    </div>
+    <div id="footer-ad" class="hidden">
+        <ins class="adsbygoogle footer-ad"
+            data-ad-client="{{ $adSenseClient }}"
+            data-ad-slot="{{ config('services.google.ad_sense.unit_footer') }}"
+            data-ad-format="auto">
+        </ins>
+    </div>
     @endif
 
     @if ($bsaCpcCode = config('services.bsa.cpc_code'))
