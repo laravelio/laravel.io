0a0ec4536bd5778aa471536ffcbb821c7ea78f3d
Dries Vints <dries.vints@gmail.com>
2019-12-03 11:09:07 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
