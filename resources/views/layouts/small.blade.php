4714942913b0edff0d2a1822073db6d08389a33a
Joe Dixon <hello@joedixon.co.uk>
2020-02-05 08:15:36 +0000

    Adjust login width on xl screens

--- resources/views/layouts/small.blade.php
+++ resources/views/layouts/small.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <div class="flex items-center justify-center my-8 md:my-20">
-        <div class="w-full md:w-1/2 lg:w-1/3">
+        <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
             <h1 class="text-4xl text-gray-700 text-center mb-2 font-bold">{{ $title }}</h1>
             <div class="p-8 md:border-2 md:rounded md:bg-gray-100">
                 @include('layouts._alerts')
