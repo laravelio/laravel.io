362377acd53c972dd831695ca2821d363cb92e0d
Joe Dixon <hello@joedixon.co.uk>
2020-02-07 08:15:30 +0000

    Fix last thread-card selector

--- resources/views/forum/overview.blade.php
+++ resources/views/forum/overview.blade.php
@@ -19,50 +19,55 @@
     <div class="container mx-auto px-4 pt-4 flex flex-wrap flex-col-reverse md:flex-row">
         <div class="w-full md:w-3/4 md:pr-3">
             @if (count($threads))
-                @foreach ($threads as $thread)
-                    <div class="thread-card">
-                        <a href="{{ route('thread', $thread->slug()) }}">
-                            <h4 class="flex justify-between text-xl font-bold text-gray-900">
-                                {{ $thread->subject() }}
-                                <span class="text-base font-normal">
-                                    <i class="fa fa-comment text-gray-500 mr-2"></i>
-                                    {{ count($thread->replies()) }}
-                                </span>
-                            </h4>
-                            <p class="text-gray-600" v-pre>{!! $thread->excerpt() !!}</p>
-                        </a>
-                        <div class="flex flex-col justify-between md:flex-row md:items-center text-sm pt-5">
-                            <div class="flex flex-col md:flex-row md:items-center">
-                                <div class="flex mb-4 md:mb-0">
-                                    @if (count($thread->replies()))
-                                        @include('forum.threads.info.avatar', ['user' => $thread->replies()->last()->author()])
-                                    @else
-                                        @include('forum.threads.info.avatar', ['user' => $thread->author()])
-                                    @endif
-
-                                    <div class="mr-6 text-gray-700">
+                <div>
+                    @foreach ($threads as $thread)
+                        <div class="thread-card">
+                            <a href="{{ route('thread', $thread->slug()) }}">
+                                <h4 class="flex justify-between text-xl font-bold text-gray-900">
+                                    {{ $thread->subject() }}
+                                    <span class="text-base font-normal">
+                                        <i class="fa fa-comment text-gray-500 mr-2"></i>
+                                        {{ count($thread->replies()) }}
+                                    </span>
+                                </h4>
+                                <p class="text-gray-600">{!! $thread->excerpt() !!}</p>
+                            </a>
+                            <div class="flex flex-col justify-between md:flex-row md:items-center text-sm pt-5">
+                                <div class="flex flex-col md:flex-row md:items-center">
+                                    <div class="flex mb-4 md:mb-0">
                                         @if (count($thread->replies()))
-                                            @php($lastReply = $thread->replies()->last())
-                                            <a href="{{ route('profile', $lastReply->author()->username()) }}" class="text-green-darker mr-2">{{ $lastReply->author()->name() }}</a> replied
-                                            {{ $lastReply->createdAt()->diffForHumans() }}
+                                            @include('forum.threads.info.avatar', ['user' => $thread->replies()->last()->author()])
                                         @else
-                                            <a href="{{ route('profile', $thread->author()->username()) }}" class="text-green-darker mr-2">{{ $thread->author()->name() }}</a> posted
-                                            {{ $thread->createdAt()->diffForHumans() }}
+                                            @include('forum.threads.info.avatar', ['user' => $thread->author()])
                                         @endif
-                                    </div>
-                                </div>
-                                @include('forum.threads.info.tags')
-                            </div>
 
-                            @if ($thread->isSolved())
-                                <a class="label label-primary text-center mt-4 md:mt-0" href="{{ route('thread', $thread->slug()) }}#{{ $thread->solutionReplyRelation->id }}">
-                                    <i class="fa fa-check mr-2"></i>
-                                    View solution
-                                </a>
-                            @endif
+                                        <div class="mr-6 text-gray-700">
+                                            @if (count($thread->replies()))
+                                                @php($lastReply = $thread->replies()->last())
+                                                <a href="{{ route('profile', $lastReply->author()->username()) }}" class="text-green-darker mr-2">{{ $lastReply->author()->name() }}</a> replied
+                                                {{ $lastReply->createdAt()->diffForHumans() }}
+                                            @else
+                                                <a href="{{ route('profile', $thread->author()->username()) }}" class="text-green-darker mr-2">{{ $thread->author()->name() }}</a> posted
+                                                {{ $thread->createdAt()->diffForHumans() }}
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    @include('forum.threads.info.tags')
+
+                                </div>
+
+                                @if ($thread->isSolved())
+                                    <a class="label label-primary text-center mt-4 md:mt-0" href="{{ route('thread', $thread->slug()) }}#{{ $thread->solutionReplyRelation->id }}">
+                                        <i class="fa fa-check mr-2"></i>
+                                        View solution
+                                    </a>
+                                @endif
+
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
 
                 <div class="flex justify-center">
                     {!! $threads->render() !!}
