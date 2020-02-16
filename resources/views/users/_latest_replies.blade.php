39e940f4c9830b7c93cf539fea3900c7df3d98e3
Joe Dixon <hello@joedixon.co.uk>
2020-02-16 09:43:40 +0000

    Add relative timestamp to replies

--- resources/views/users/_latest_replies.blade.php
+++ resources/views/users/_latest_replies.blade.php
@@ -14,18 +14,28 @@
                     </a>
 
                     <div>
-                        @if ($reply->replyAble()->isSolutionReply($reply))
-                            <span class="label label-primary text-center mt-4 md:mt-0 mr-2">
-                                <i class="fa fa-check mr-2"></i>
-                                Solution
-                            </span>
-                        @endif
-
                         <span class="text-base font-normal">
                             <i class="fa fa-thumbs-up text-gray-500 mr-1"></i>
                             {{ $reply->likes_count }}
                         </span>
                     </div>
+                </div>
+
+                <div class="flex flex-col justify-between md:flex-row md:items-center text-sm pt-5">
+                    <div class="flex flex-col md:flex-row md:items-center">
+                        <div class="flex mb-4 md:mb-0">
+                            <div class="mr-6 text-gray-700">
+                                Posted {{ $reply->createdAt()->diffForHumans() }}
+                            </div>
+                        </div>
+                    </div>
+
+                    @if ($reply->replyAble()->isSolutionReply($reply))
+                        <span class="label label-primary text-center mt-4 md:mt-0">
+                            <i class="fa fa-check mr-2"></i>
+                            Solution
+                        </span>
+                    @endif
                 </div>
             </div>
         @empty
