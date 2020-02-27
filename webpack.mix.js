11a5086e998b18da38a2b96e4853a2432aaeb61b
Dries Vints <dries.vints@gmail.com>
2020-02-27 21:56:40 +0100

    Re-enable JS tracking and sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,13 @@
 const mix = require('laravel-mix');
 const FlareWebpackPluginSourcemap = require("@flareapp/flare-webpack-plugin-sourcemap");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
+    .webpackConfig({
+        plugins: [
+            new FlareWebpackPluginSourcemap({ key: process.env.MIX_FLARE_KEY })
+        ],
+    })
+    .sourceMaps(true, 'hidden-source-map')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
