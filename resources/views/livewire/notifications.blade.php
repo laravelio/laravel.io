485f37ee86645bdb0c06d8c6498a87a4c61258f6
Joe Dixon <hello@joedixon.co.uk>
2020-02-29 15:39:29 +0000

    Add tailwind UI notifications

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -1,19 +1,25 @@
 <div>
     @if($notifications->count())
-        <table class="table bg-white">
-            <tbody>
-                @foreach($notifications as $notification)
-                    @includeIf(
-                        "users.notifications.{$notification->data['type']}", 
-                        ['data' => $notification->data]
-                    )
-                @endforeach
-            </tbody>
-        </table>
+        <div class="flex flex-col">
+            <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                    <table class="min-w-full">
+                        <tbody>
+                            @foreach($notifications as $notification)
+                                @includeIf(
+                                    "users.notifications.{$notification->data['type']}", 
+                                    ['data' => $notification->data]
+                                )
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
         {{ $notifications->links() }}
     @else
-        <p class="text-gray-600 text-lg">
+        <p class="text-gray-600 text-base">
             You have no unread notifications
         </p>
     @endif
--- resources/views/users/notifications/reply.blade.php
+++ resources/views/users/notifications/reply.blade.php
@@ -1,18 +1,28 @@
 <tr>
-    <td><i class="fa fa-reply text-gray-500"></i></td>
-    <td>
-        @if($data['thread']['author_id'] == Auth::id())
-            <a href="{{ route('profile', $data['author']['username']) }}" class="text-green-darker">{{ $data['author']['name'] }}</a> replied to your <a href="{{ route('thread', $data['thread']['slug']) }}" class="text-green-darker">thread</a>
-        @else
-            <a href="{{ route('profile', $data['author']['username']) }}" class="text-green-darker">{{ $data['author']['name'] }}</a> replied to a <a href="{{ route('thread', $data['thread']['slug']) }}" class="text-green-darker">thread</a> you're subscribed to
-        @endif
+    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-600">
+        <div class="flex items-center">
+            <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-4">
+                <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
+            </svg>
+            <div>
+                @if($data['thread']['author_id'] == Auth::id())
+                    <a href="{{ route('profile', $data['author']['username']) }}" class="text-green-darker">{{ $data['author']['name'] }}</a> replied to your <a href="{{ route('thread', $data['thread']['slug']) }}" class="text-green-darker">thread</a>
+                @else
+                    <a href="{{ route('profile', $data['author']['username']) }}" class="text-green-darker">{{ $data['author']['name'] }}</a> replied to a <a href="{{ route('thread', $data['thread']['slug']) }}" class="text-green-darker">thread</a> you're subscribed to
+                @endif
+            </div>
+        </div>
     </td>
-    <td>
+    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-600">
         {{ $notification->created_at->diffForHumans() }}
     </td>
-    <td>
-        <button wire:click="markAsRead('{{ $notification->id }}')">
-            <i class="fa fa-check"></i>
-        </button>
+    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-600 text-right">
+        <div class="flex justify-end">
+            <button wire:click="markAsRead('{{ $notification->id }}')" class="text-green-primary">
+                <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
+                </svg>
+            </button>
+        </div>
     </td>
 </tr>
