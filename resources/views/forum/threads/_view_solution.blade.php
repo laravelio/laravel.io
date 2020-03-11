eef2f068d4bc795f7b4670af2576dfdc2b1873a8
Ryan Chandler <ryan@chandler.codes>
2020-03-11 19:52:04 +0000

    Updated styling to fix dodgy tag alignment

--- resources/views/forum/threads/_view_solution.blade.php
+++ resources/views/forum/threads/_view_solution.blade.php
@@ -1,5 +1,5 @@
 @if ($thread->isSolved())
-    <a class="label label-primary text-center mt-4 md:mt-0"
+    <a class="label label-primary text-center mt-4 sm:mt-0"
        href="{{ route('thread', $thread->slug()) }}#{{ $thread->solution_reply_id }}">
         <i class="fa fa-check mr-2"></i>
         View solution
