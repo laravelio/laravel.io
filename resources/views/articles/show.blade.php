ae106db9df2b9fc742efebf4e00c4ca7af2ce509
Joe Dixon <hello@joedixon.co.uk>
2020-04-24 11:05:04 +0100

    Formatting

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="max-w-screen-md mx-auto p-4 pt-8">
         <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none mb-4">{{ $article->title() }}</h1>
-        @if($article->isNotPublished())
+        @if ($article->isNotPublished())
             <span class="label inline-flex mb-4">
                 Draft
             </span>
