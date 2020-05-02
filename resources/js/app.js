d97617115596c1f0a324e0116263a786e2a06d82
Dries Vints <dries@vints.io>
2020-05-02 13:03:09 +0200

    Implement Prettier and workflows

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,29 @@
-import 'alpinejs'
-import hljs from "highlight.js";
-import Choices from "choices.js";
+import 'alpinejs';
+import hljs from 'highlight.js';
+import Choices from 'choices.js';
 
-import "highlight.js/styles/github.css";
-import "choices.js/public/assets/styles/choices.css";
+import 'highlight.js/styles/github.css';
+import 'choices.js/public/assets/styles/choices.css';
 
 require('./bootstrap');
 require('./search');
 
 // Create a multiselect element.
-window.choices = (element) => { return new Choices(element, { maxItemCount: 3, removeItemButton: true }) };
+window.choices = (element) => {
+    return new Choices(element, { maxItemCount: 3, removeItemButton: true });
+};
 
 // Syntax highlight code blocks.
 window.highlightCode = (element) => {
-    element.querySelectorAll("pre code").forEach(block => {
+    element.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block);
     });
-}
+};
 
 // Handle the click event of the style buttons inside the editor.
 window.handleClick = (style, element) => {
     const { styles } = editorConfig();
-    const input = element.querySelectorAll("textarea")[0];
+    const input = element.querySelectorAll('textarea')[0];
 
     // Get the start and end positions of the current selection.
     const selectionStart = input.selectionStart;
@@ -31,91 +33,76 @@ window.handleClick = (style, element) => {
     const styleFormat = styles[style];
 
     // Get any prefix and/or suffix characters from the selected style.
-    const prefix = styleFormat.before ? styleFormat.before : "";
-    const suffix = styleFormat.after ? styleFormat.after : "";
+    const prefix = styleFormat.before ? styleFormat.before : '';
+    const suffix = styleFormat.after ? styleFormat.after : '';
 
     // Insert the prefix at the relevant position.
-    input.value = insertCharactersAtPosition(
-        input.value,
-        prefix,
-        selectionStart
-    );
+    input.value = insertCharactersAtPosition(input.value, prefix, selectionStart);
 
     // Insert the suffix at the relevant position.
-    input.value = insertCharactersAtPosition(
-        input.value,
-        suffix,
-        selectionEnd + prefix.length
-    );
+    input.value = insertCharactersAtPosition(input.value, suffix, selectionEnd + prefix.length);
 
     // Reselect the selection and focus the input.
-    input.setSelectionRange(
-        selectionStart + prefix.length,
-        selectionEnd + prefix.length
-    );
+    input.setSelectionRange(selectionStart + prefix.length, selectionEnd + prefix.length);
     input.focus();
-}
+};
 
 // Insert provided characters at the desired place in a string.
 const insertCharactersAtPosition = (string, character, position) => {
-    return [
-        string.slice(0, position),
-        character,
-        string.slice(position)
-    ].join("");
-}
+    return [string.slice(0, position), character, string.slice(position)].join('');
+};
 
 // Configuration object for the text editor.
 window.editorConfig = () => {
     return {
         styles: {
             header: {
-                before: "### ",
+                before: '### ',
                 class: {
-                    "fa-header": true
-                }
+                    'fa-header': true,
+                },
             },
             bold: {
-                before: "**",
-                after: "**",
+                before: '**',
+                after: '**',
                 class: {
-                    "fa-bold": true
-                }
+                    'fa-bold': true,
+                },
             },
             italic: {
-                before: "_",
-                after: "_",
+                before: '_',
+                after: '_',
                 class: {
-                    "fa-italic": true
-                }
+                    'fa-italic': true,
+                },
             },
             quote: {
-                before: "> ",
+                before: '> ',
                 class: {
-                    "fa-quote-left": true
-                }
+                    'fa-quote-left': true,
+                },
             },
             code: {
-                before: "`",
-                after: "`",
+                before: '`',
+                after: '`',
                 class: {
-                    "fa-code": true
-                }
+                    'fa-code': true,
+                },
             },
             link: {
-                before: "[](",
-                after: ")",
+                before: '[](',
+                after: ')',
                 class: {
-                    "fa-link": true
-                }
+                    'fa-link': true,
+                },
             },
             image: {
-                before: "![](",
-                after: ")",
+                before: '![](',
+                after: ')',
                 class: {
-                    "fa-file-image-o": true
-                }
-            }
-        }
-    }
-}
+                    'fa-file-image-o': true,
+                },
+            },
+        },
+    };
+};
--- resources/js/search.js
+++ resources/js/search.js
@@ -1,19 +1,17 @@
 import algoliasearch from 'algoliasearch/lite';
 
-const client = algoliasearch(process.env.MIX_ALGOLIA_APP_ID, process.env.MIX_ALGOLIA_SECRET)
-const index = client.initIndex(process.env.MIX_ALGOLIA_INDEX)
+const client = algoliasearch(process.env.MIX_ALGOLIA_APP_ID, process.env.MIX_ALGOLIA_SECRET);
+const index = client.initIndex(process.env.MIX_ALGOLIA_INDEX);
 
 window.search = (event) => {
     // If the input is empty, return no results.
     if (event.target.value.length === 0) {
-        return Promise.resolve({ hits: [] })
+        return Promise.resolve({ hits: [] });
     }
 
     // Perform the search using the provided input.
     return index.search(event.target.value, {
         hitsPerPage: 5,
-        attributesToSnippet: [
-            'body:10'
-        ]
+        attributesToSnippet: ['body:10'],
     });
-}
+};
