72e5bc5d420ba2f299cffd3149bfdcbdeaa9a044
Dries Vints <dries@vints.io>
2020-05-02 16:32:06 +0200

    Revert testcase change

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,13 +10,6 @@ abstract class TestCase extends IlluminateTestCase
     use CreatesUsers;
     use HttpAssertions;
 
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->withoutMix();
-    }
-
     protected function dispatch($job)
     {
         return $job->handle();
