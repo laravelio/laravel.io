4f59443c06f4f7e1a2d0e0361fe2eb464628df99
Joe Dixon <hello@joedixon.co.uk>
2020-05-03 15:18:11 +0100

    Append app name to title

--- resources/views/layouts/_social.blade.php
+++ resources/views/layouts/_social.blade.php
@@ -1,11 +1,11 @@
 <!-- Twitter sharing -->
 <meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:title" content="{{ $title ?? config('app.name') }}">
+<meta name="twitter:title" content="{{ isset($title) ? $title.' | ' : '' }}{{ config('app.name') }}">
 <meta name="twitter:creator" content="@laravelio" />
 <!-- /Twitter sharing -->
 
 <!-- Facebook sharing -->
 <meta property="og:url" content="{{ url()->current() }}" />
-<meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+<meta property="og:title" content="{{ isset($title) ? $title.' | ' : '' }}{{ config('app.name') }}" />
 <meta property="og:image" content="{{ asset('images/laravelio-share.png') }}" />
 <!-- /Facebook sharing -->
