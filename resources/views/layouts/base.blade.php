7e084eeebba93187b0dc21164daf92972686287a
Joe Dixon <hello@joedixon.co.uk>
2020-05-03 15:19:51 +0100

    Remove additional space

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ isset($title) ? $title.' | ' : '' }} {{ config('app.name') }}</title>
+    <title>{{ isset($title) ? $title.' | ' : '' }}{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
