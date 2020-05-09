75a1d15dd262a632d58476f9ce2b568d75343603
Dries Vints <dries@vints.io>
2020-05-09 16:07:01 +0200

    Update tracking code

--- resources/views/layouts/_fathom.blade.php
+++ resources/views/layouts/_fathom.blade.php
@@ -1,5 +1,5 @@
 @if (App::environment('production'))
     <!-- Fathom - beautiful, simple website analytics -->
-    <script src="https://cdn.usefathom.com/3.js" site="UXCUXOED"></script>
+    <script src="https://earthworm.laravel.io/script.js" site="UXCUXOED" defer></script>
     <!-- / Fathom -->
 @endif
