45f23ecfb617c9a35ed82bdc22246e92867fed54
Joe Dixon <hello@joedixon.co.uk>
2020-05-12 08:21:32 +0100

    Add articles to nav

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="bg-white shadow">
-    <div class="max-w-6xl mx-auto px-2 sm:px-4 lg:px-8">
+    <div class="container mx-auto px-4">
         <div class="flex justify-between h-16">
             <div class="flex px-2 lg:px-0">
                 <div class="flex-shrink-0 flex items-center">
@@ -11,6 +11,9 @@
                 <div class="hidden lg:ml-6 lg:flex">
                     <a href="{{ route('forum') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out {{ active(['forum', 'threads*', 'thread']) }}">
                         Forum
+                    </a>
+                    <a href="{{ route('articles') }}" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out {{ active(['articles', 'articles*']) }}">
+                        Articles
                     </a>
                     <a href="https://paste.laravel.io" class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                         Pastebin
@@ -132,7 +135,12 @@
     </div>
     <div :class="{'block': open, 'hidden': !open}" class="hidden lg:hidden">
         <div class="pt-2 pb-3">
-            <a href="{{ route('forum') }}" class="{{ active(['forum', 'threads*', 'thread']) }} block pl-3 pr-4 py-2 border-l-4 text-base font-medium bg-indigo-50 focus:outline-none transition duration-150 ease-in-out">Forum</a>
+            <a href="{{ route('forum') }}" class="{{ active(['forum', 'threads*', 'thread']) }} block pl-3 pr-4 py-2 border-l-4 text-base font-medium bg-indigo-50 focus:outline-none transition duration-150 ease-in-out">
+                Forum
+            </a>
+            <a href="{{ route('articles') }}" class="{{ active(['articles', 'articles*']) }} block pl-3 pr-4 py-2 border-l-4 text-base font-medium bg-indigo-50 focus:outline-none transition duration-150 ease-in-out">
+                Articles
+            </a>
             <a href="https://paste.laravel.io" class="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">Pastebin</a>
 
             <div class="mt-1">
