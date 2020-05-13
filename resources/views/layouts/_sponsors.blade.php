5321c994eb9af6a5178aebe0f38e14009db0f559
Dries Vints <dries@vints.io>
2020-05-13 16:54:45 +0200

    Fixup sponsors

--- resources/views/layouts/_sponsors.blade.php
+++ resources/views/layouts/_sponsors.blade.php
@@ -25,12 +25,12 @@
                     <img class="max-h-12" src="{{ asset('images/sponsors/forge.png') }}" alt="Forge">
                 </a>
             </div>
-            <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1 px-2">
+            <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 px-2">
                 <a class="flex items-center" href="https://envoyer.io/">
                     <img class="max-h-12" src="{{ asset('images/sponsors/envoyer.png') }}" alt="Envoyer">
                 </a>
             </div>
-            <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1 px-2">
+            <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1 px-2">
                 <a class="flex items-center" href="https://blackfire.io/">
                     <img class="max-h-12" src="{{ asset('images/sponsors/blackfire-io.png') }}" alt="Blackfire.io">
                 </a>
