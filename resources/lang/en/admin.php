5289a02b3eded49a5ea386b1b578bedc35b4bb3c
Joe Dixon <hello@joedixon.co.uk>
2020-06-07 20:52:14 +0100

    Fix typo

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -6,5 +6,5 @@ return [
     'users.unbanned' => ':0 was unbanned!',
     'users.deleted' => ':0 was deleted and all of their content was removed!',
     'articles.approved' => 'The article has been approved and is live on the site.',
-    'articles.approved' => 'The article has been disapproved and removed from the site.',
+    'articles.disapproved' => 'The article has been disapproved and removed from the site.',
 ];
