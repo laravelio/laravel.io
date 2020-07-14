03269ace2343a2b81bf20c7650d88898e1878ca8
Joe Dixon <hello@joedixon.co.uk>
2020-07-14 10:13:50 +0100

    Add route to generate social image

---
+++ app/Http/Controllers/SocialImageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\GenerateSocialShareImage;
+use App\Models\Article;
+
+class SocialImageController extends Controller
+{
+    public function __invoke(Article $article)
+    {
+        return $this->dispatchNow(new GenerateSocialShareImage($article));
+    }
+}
