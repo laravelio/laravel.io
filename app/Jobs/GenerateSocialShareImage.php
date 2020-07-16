17991528924d21a2ab7c15e3af01545f23a02228
joedixon <joedixon@users.noreply.github.com>
2020-07-16 13:45:41 +0000

    Apply PHP CS Fixer changes

--- app/Jobs/GenerateSocialShareImage.php
+++ app/Jobs/GenerateSocialShareImage.php
@@ -35,9 +35,9 @@ final class GenerateSocialShareImage
         $text = wordwrap($this->article->title(), self::CHARACTERS_PER_LINE);
 
         return $image->cache(function ($image) use ($text) {
-            $image->make(resource_path('images/'.self::TEMPLATE))
+            $image->make(resource_path('images/' . self::TEMPLATE))
                 ->text($text, self::TEXT_X_POSITION, self::TEXT_Y_POSITION, function ($font) {
-                    $font->file(resource_path('fonts/'.self::FONT));
+                    $font->file(resource_path('fonts/' . self::FONT));
                     $font->size(self::FONT_SIZE);
                     $font->color(self::TEXT_COLOUR);
                 });
