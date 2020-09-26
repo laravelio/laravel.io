b7b410e653201f3b3d11b3e92d268db9afc59a63
Dries Vints <dries@vints.io>
2020-09-26 10:58:07 +0200

    Add new Metomic

--- resources/views/layouts/_metomic.blade.php
+++ resources/views/layouts/_metomic.blade.php
@@ -1,4 +1,4 @@
 @if (App::environment('production'))
-    <script src="https://config.metomic.io/config.js?id=prj:c2b4318e-bdb0-47f2-8dc6-279424a5ff9c" crossorigin charset="utf-8"></script>
+    <script src="https://config.metomic.io/config.js?id=prj:bae31775-a83e-4b64-8df7-98b6eadf0a02" crossorigin charset="utf-8"></script>
     <script src="https://consent-manager.metomic.io/embed.js" crossorigin charset="utf-8"></script>
 @endif
