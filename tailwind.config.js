bd4ebcee3af41af860af6ee866e1eed643604263
Dries Vints <dries.vints@gmail.com>
2020-11-03 09:37:42 +0100

    CSS & JS Styling (#562)
    * Update .styleci.yml
    
    * Apply fixes from StyleCI
    
    Co-authored-by: Dries Vints <driesvints@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        green: {
-          light: '#baebe1',
-          primary: '#18bc9c',
-          dark: '#15a589',
-          darker: '#12826c',
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    light: '#baebe1',
+                    primary: '#18bc9c',
+                    dark: '#15a589',
+                    darker: '#12826c',
+                },
+                red: {
+                    primary: '#e53e3e',
+                    dark: '#c53030',
+                },
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
         },
-        red: {
-          primary: '#e53e3e',
-          dark: '#c53030',
-        }
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui'),
-  ],
+    variants: {},
+    plugins: [require('@tailwindcss/ui')],
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,10 @@
 const mix = require('laravel-mix');
-const FlareWebpackPluginSourcemap = require("@flareapp/flare-webpack-plugin-sourcemap");
+const FlareWebpackPluginSourcemap = require('@flareapp/flare-webpack-plugin-sourcemap');
 
 mix.js('resources/js/app.js', 'public/js')
     .webpackConfig({
-        plugins: [
-            new FlareWebpackPluginSourcemap({ key: process.env.MIX_FLARE_KEY })
-        ],
+        plugins: [new FlareWebpackPluginSourcemap({ key: process.env.MIX_FLARE_KEY })],
     })
     .sourceMaps(true, 'hidden-source-map')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
+    .postCss('resources/css/app.css', 'public/css', [require('postcss-import'), require('tailwindcss')])
     .version();
