4d1b2af5c54dc868c53bf9fe486f87f7ea013c20
Joe Dixon <hello@joedixon.co.uk>
2020-11-05 08:59:14 +0000

    Remove down method

--- database/migrations/2020_11_03_205735_add_uuid_to_failed_jobs_table.php
+++ database/migrations/2020_11_03_205735_add_uuid_to_failed_jobs_table.php
@@ -1,32 +1,15 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddUuidToFailedJobsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('failed_jobs', function (Blueprint $table) {
             $table->string('uuid')->after('id')->nullable()->unique();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('failed_jobs', function (Blueprint $table) {
-            $table->dropColumn('uuid');
         });
     }
 }
