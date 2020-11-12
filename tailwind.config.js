75885ab7ca1fcaf4ee862159def725e32234aefc
Joe Dixon <joedixon@users.noreply.github.com>
2020-11-12 20:14:09 +0000

    Apply fixes from StyleCI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,16 @@ module.exports = {
         extend: {
             colors: {
                 green: {
-                    100: "#d1f2eb",
-                    200: "#a3e4d7",
-                    300: "#74d7c4",
-                    400: "#46c9b0",
+                    100: '#d1f2eb',
+                    200: '#a3e4d7',
+                    300: '#74d7c4',
+                    400: '#46c9b0',
                     500: '#18bc9c',
-                    600: "#15a589",
-                    700: "#0e715e",
-                    800: "#0a4b3e",
-                    900: "#05261f",
-                }
+                    600: '#15a589',
+                    700: '#0e715e',
+                    800: '#0a4b3e',
+                    900: '#05261f',
+                },
             },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
