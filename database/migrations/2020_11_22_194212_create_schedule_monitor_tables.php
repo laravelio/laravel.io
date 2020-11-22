2713330cea56c0c4a63741aaae6d8fb43f142e2c
Dries Vints <driesvints@users.noreply.github.com>
2020-11-22 19:51:57 +0000

    Apply fixes from StyleCI

--- database/migrations/2020_11_22_194212_create_schedule_monitor_tables.php
+++ database/migrations/2020_11_22_194212_create_schedule_monitor_tables.php
@@ -29,7 +29,6 @@ class CreateScheduleMonitorTables extends Migration
             $table->timestamps();
         });
 
-
         Schema::create('monitored_scheduled_task_log_items', function (Blueprint $table) {
             $table->bigIncrements('id');
 
