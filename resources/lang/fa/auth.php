c2d878e25b253222c9438444203ba9e8c862c613
Saeed Rostami <privatemx.2010@gmail.com>
2020-12-10 20:05:10 +0330

    Update auth.php

--- resources/lang/fa/auth.php
+++ resources/lang/fa/auth.php
@@ -1,1 +1,28 @@
 
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'مشخصات وارد شده با اطلاعات ما سازگار نیست.',
+    'throttle' => 'دفعات تلاش شما برای ورود بیش از حد مجاز است. لطفا پس از :seconds ثانیه مجددا تلاش فرمایید.',
+    'password' => 'رمز عبور شما معتبر نیست.',
+
+    'verification' => [
+        'sent' => 'ایمیل تایید ارسال شد به  :0.  شما میتوانید ایمیل خود را در تنظیمات پروفایل تغییر دهید',
+        'already_verified' => 'ایمیل شما قبلا تایید شده است.',
+        'success' => 'ایمیل شما به درستی مورد تایید قرار گرفت',
+        'no_match' => 'موفق به تایید ایمیل شما نشدیم. آدرس ایمیل و کد تایید مطابقت ندارد',
+    ],
+
+];
