00de7fc1a7619128c8c5a35a4fc53a9ab1802868
Saeed Rostami <privatemx.2010@gmail.com>
2020-12-10 20:25:21 +0330

    Create forum.php

---
+++ resources/lang/fa/forum.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'threads.created' => 'موضوع با موفقیت ایجاد شد!',
+    'threads.updated' => 'موضوع با موفقیت تغییر یافت!',
+    'threads.deleted' => 'موضوع با موفقیت حذف شد!',
+
+];
