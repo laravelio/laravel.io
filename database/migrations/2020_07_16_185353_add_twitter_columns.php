918910015e5ea03c913f89b74ba5dd123b6daa99
Joe Dixon <hello@joedixon.co.uk>
2020-12-12 14:23:47 +0000

    Rename migration

--- database/migrations/2020_07_16_185353_add_twitter_columns.php
+++ database/migrations/2020_07_16_185353_add_twitter_columns.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateArticlesAndUsersTablesToIncludeShareInformation extends Migration
+class AddTwitterColumns extends Migration
 {
     public function up()
     {
