037ce649acb674f4f8bf15f6abff95b4adc276ca
Joe Dixon <hello@joedixon.co.uk>
2020-12-12 14:24:08 +0000

    Format template

--- resources/views/articles/_form.blade.php
+++ resources/views/articles/_form.blade.php
@@ -130,9 +130,12 @@
                 </span>
             @endif
         </div>
+
         @unless (Auth::user()->twitter())
             <span class="text-gray-600 text-sm mt-4 block">
-                Articles will be shared on Twitter. <a href="{{ route('settings.profile') }}" class="text-green-darker">Add your Twitter handle</a> and we'll include that too.
+                Articles will be shared on Twitter. 
+                <a href="{{ route('settings.profile') }}" class="text-green-darker">Add your Twitter handle</a> 
+                and we'll include that too.
             </span>
         @endunless
     </div>
