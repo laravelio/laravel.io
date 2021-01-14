da15f7df41f038aaf29e1988f8ed0946884c7927
Dries Vints <dries@vints.io>
2021-01-14 21:40:13 +0100

    Remove Flare JS tracking

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,6 @@
 const mix = require('laravel-mix');
-const FlareWebpackPluginSourcemap = require('@flareapp/flare-webpack-plugin-sourcemap');
 
 mix.js('resources/js/app.js', 'public/js')
-    .webpackConfig({
-        plugins: [new FlareWebpackPluginSourcemap({ key: process.env.MIX_FLARE_KEY })],
-    })
     .sourceMaps(true, 'hidden-source-map')
     .postCss('resources/css/app.css', 'public/css', [require('postcss-import'), require('tailwindcss')])
     .version();
