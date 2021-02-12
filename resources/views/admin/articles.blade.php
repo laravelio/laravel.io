5df0f4dd78889dc9b3ca008ee74abb4286b7a29b
Joe Dixon <hello@joedixon.co.uk>
2021-02-12 11:54:44 +0000

    Use full container

--- resources/views/admin/articles.blade.php
+++ resources/views/admin/articles.blade.php
@@ -9,7 +9,7 @@
         </div>
     </div>
 
-    <main class="max-w-7xl mx-auto pb-10 lg:py-12 lg:px-8">
+    <main class="container mx-auto pb-10 lg:py-12 sm:px-4">
         <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
             <aside class="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-2">
                 @include('admin.partials._navigation')
