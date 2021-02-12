56c9a6e0268a9b7a2581c068fd793b6967f50e74
Joe Dixon <hello@joedixon.co.uk>
2021-02-12 10:50:20 +0000

    Persist filter

--- resources/views/forum/_tags.blade.php
+++ resources/views/forum/_tags.blade.php
@@ -2,12 +2,20 @@
     Tags
 </p>
 <div class="mt-3 space-y-2" aria-labelledby="tags-menu">
-    <a href="{{ route('forum') }}" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
-        All
+    <a 
+        href="{{ route('forum', ['filter' => $filter]) }}" 
+        class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-white {{ (! isset($activeTag) ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
+    >
+        <span class="truncate">
+            All
+        </span>
     </a>
 
     @foreach (App\Models\Tag::orderBy('name')->get() as $tag)
-        <a href="{{ route('forum.tag', $tag->slug()) }} "class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
+        <a 
+            href="{{ route('forum.tag', [$tag->slug(), 'filter' => $filter]) }}" 
+            class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-white {{ (isset($activeTag) && $activeTag->id() === $tag->id() ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
+        >
             <span class="truncate">
                 {{ $tag->name() }}
             </span>
