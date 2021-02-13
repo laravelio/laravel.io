389b7dc350e3d3d1dbfe8381f5999ea019503fc7
Dries Vints <dries@vints.io>
2021-02-13 18:22:21 +0100

    Update articles page

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,44 +4,49 @@
 @extends('layouts.default')
 
 @push('meta')
-<link rel="canonical" href="{{ $article->canonicalUrl() }}" />
+    <link rel="canonical" href="{{ $article->canonicalUrl() }}" />
 @endpush
 
 @section('content')
     <div class="relative pt-16 bg-white">
         <div class="text-lg max-w-prose mx-auto">
             <h1>
-                <span class="block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <span class="block text-3xl sm:text-4xl text-center leading-8 font-extrabold tracking-tight text-gray-900">
                     {{ $article->title() }}
                 </span>
             </h1>
         </div>
 
-        <div class="flex flex-col items-center mt-6 pb-6 border-b">
+        <div class="flex flex-col items-center mt-8">
             <div class="flex items-center justify-center">
                 <div class="flex-shrink-0">
                     <a href="{{ route('profile', $article->author()->username()) }}">
                         <span class="sr-only">
                             {{ $article->author()->name() }}
                         </span>
+
                         <x-avatar :user="$article->author()" class="h-10 w-10 rounded-full" />
                     </a>
                 </div>
+
                 <div class="ml-3">
                     <p class="text-sm font-medium text-gray-900">
                         <a href="{{ route('profile', $article->author()->username()) }}" class="hover:underline">
                             {{ $article->author()->name() }}
                         </a>
                     </p>
+
                     <div class="flex space-x-1 text-sm text-gray-500">
                         @if ($article->isPublished())
                             <time datetime="{{ $article->submittedAt()->format('Y-m-d') }}">
                                 {{ $article->submittedAt()->format('j M, Y') }}
                             </time>
+
                             <span aria-hidden="true">
                                 &middot;
                             </span>
                         @endif
+
                         <span>
                             {{ $article->readTime() }} min read
                         </span>
@@ -50,7 +55,7 @@
             </div>
 
             @if ($article->isNotPublished())
-                <div class="mt-4">
+                <div class="mt-6">
                     <x-badges.badge>
                         @if ($article->isAwaitingApproval())
                             Awaiting Approval
@@ -58,29 +63,31 @@
                             Draft
                         @endif
                     </x-badges.badge>
-                    @else
                 </div>
             @endif
         </div>
 
         <div class="flex flex-col md:flex-row mx-auto justify-center bg-white">
-            <div class="md:sticky md:top-0 md:self-start pt-2 md:pt-6 w-full md:w-16">
+            <div class="md:sticky md:top-0 md:self-start pt-6 md:pt-14 w-full md:w-16">
                 @include('articles._sidebar')
             </div>
+
             <div
-                class="prose prose-lg text-gray-500 prose-lio px-4 pt-2 md:pt-6"
-                x-data="{}" 
-                    x-data="{}" 
-                x-data="{}" 
+                x-data="{}"
                 x-init="function () { highlightCode($el); }"
+                class="prose prose-lg text-gray-500 prose-lio px-4"
             >
-                {!! md_to_html($article->body()) !!}
+                <x-buk-markdown>{!! $article->body() !!}</x-buk-markdown>
+
                 <div class="flex items-center pt-6 pb-10">
                     <livewire:like-article :article="$article" />
+
                     <span class="text-gray-500 text-sm ml-2">
-                        Like this article?<br>Let the author know and give them a clap!
+                        Like this article?<br>
+                        Let the author know and give them a clap!
                     </span>
                 </div>
+
                 @include('articles._series_nav')
             </div>
         </div>
