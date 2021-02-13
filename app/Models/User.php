3be3b1c648fd011827e6a6ff3d367e95f0ac6889
Dries Vints <driesvints@users.noreply.github.com>
2021-02-13 20:09:14 +0000

    Apply fixes from StyleCI

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Helpers\HasTimestamps;
 use App\Helpers\ModelHelpers;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
