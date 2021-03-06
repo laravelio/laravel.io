8c1fb283862fac6aecfab39700fc0ff7a78f5f1e
Dries Vints <dries@vints.io>
2021-03-06 20:46:23 +0100

    Remove cookie banner

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ App::getLocale() }}">
 <head>
-    @include('layouts._iubenda')
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
