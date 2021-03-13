a4b71c08e2cfd36c533d3dbbd43a9ead8080b1df
Dries Vints <dries@vints.io>
2021-03-13 11:35:57 +0100

    Reduce sizes of tags sidebar

--- resources/views/forum/_tags.blade.php
+++ resources/views/forum/_tags.blade.php
@@ -1,11 +1,11 @@
-<p class="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider" id="communities-headline">
+<p class="px-2 text-xs font-semibold text-gray-500 uppercase tracking-wider" id="communities-headline">
     Tags
 </p>
 
-<div class="mt-3 space-y-2" aria-labelledby="tags-menu">
+<div class="mt-3 space-y-1" aria-labelledby="tags-menu">
     <a 
         href="{{ route('forum', ['filter' => $filter]) }}" 
-        class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-white {{ (! isset($activeTag) ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
+        class="group flex items-center px-2 py-1 text-sm font-medium text-gray-600 hover:bg-white {{ (! isset($activeTag) ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
     >
         <span class="truncate">
             All
@@ -15,7 +15,7 @@
     @foreach ($tags as $tag)
         <a 
             href="{{ route('forum.tag', [$tag->slug(), 'filter' => $filter]) }}" 
-            class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-white {{ (isset($activeTag) && $activeTag->id() === $tag->id() ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
+            class="group flex items-center px-2 py-1 text-sm font-medium text-gray-600 hover:bg-white {{ (isset($activeTag) && $activeTag->id() === $tag->id() ? 'bg-white text-lio-500 border-lio-500 border-l-2' : '') }}"
         >
             <span class="truncate">
                 {{ $tag->name() }}
