2c39f40ff6535c7db2536fad67c602414546088f
Joe Dixon <hello@joedixon.co.uk>
2021-07-05 21:17:13 +0100

    Add link hover states

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -25,15 +25,15 @@
                         </h6>
 
                         <div class="flex flex-wrap lg:flex-col lg:flex-no-wrap">
-                            <a href="{{ route('forum') }}" class="w-1/2 text-gray-400 mb-4 lg:mb-6">
+                            <a href="{{ route('forum') }}" class="w-1/2 text-gray-400 mb-4 hover:text-gray-200 lg:mb-6">
                                 Forum
                             </a>
 
-                            <a href="{{ route('articles') }}" class="w-1/2 text-gray-400 mb-4 lg:mb-6">
+                            <a href="{{ route('articles') }}" class="w-1/2 text-gray-400 mb-4 hover:text-gray-200 lg:mb-6">
                                 Articles
                             </a>
 
-                            <a href="https://paste.laravel.io" class="w-1/2 text-gray-400 mb-4 lg:mb-6">
+                            <a href="https://paste.laravel.io" class="w-1/2 text-gray-400 mb-4 hover:text-gray-200 lg:mb-6">
                                 Pastebin
                             </a>
                         </div>
@@ -45,12 +45,12 @@
                         </h6>
 
                         <div class="flex flex-wrap lg:flex-col lg:flex-no-wrap">
-                            <a href="https://twitter.com/laravelio" class="w-1/2 text-gray-400 mb-4 lg:mb-6">
+                            <a href="https://twitter.com/laravelio" class="w-1/2 text-gray-400 mb-4 hover:text-gray-200 lg:mb-6">
                                 <x-icon-twitter class="text-white w-4 h-4 inline mr-3.5"/>
                                 Twitter
                             </a>
 
-                            <a href="https://github.com/laravelio" class="w-1/2 text-gray-400 mb-4 lg:mb-6">
+                            <a href="https://github.com/laravelio" class="w-1/2 text-gray-400 mb-4 hover:text-gray-200 lg:mb-6">
                                 <x-icon-github class="text-white w-4 h-4 inline mr-3.5"/>
                                 GitHub
                             </a>
@@ -64,24 +64,24 @@
 
                         <div class="flex flex-col flex-nowrap">
                             <div class="flex mb-4 lg:mb-6">
-                                <a href="https://laravel.com" class="w-1/2 text-gray-400">
+                                <a href="https://laravel.com" class="w-1/2 text-gray-400 hover:text-gray-200">
                                     <img src="{{ asset('images/laravel.png') }}" alt="Laravel" class="w-4 h-4 inline mr-2" />
                                     Laravel
                                 </a>
 
-                                <a href="https://laravel-news.com" class="w-1/2 text-gray-400">
+                                <a href="https://laravel-news.com" class="w-1/2 text-gray-400 hover:text-gray-200">
                                     <img src="{{ asset('images/laravel-news.png') }}" alt="Laravel News" class="w-4 h-4 inline mr-2" />
                                     Laravel News
                                 </a>
                             </div>
 
                             <div class="flex">
-                                <a href="https://laracasts.com" class="w-1/2 text-gray-400">
+                                <a href="https://laracasts.com" class="w-1/2 text-gray-400 hover:text-gray-200">
                                     <img src="{{ asset('images/laracasts.png') }}" alt="Laracasts" class="w-4 h-4 inline mr-2" />
                                     Laracasts
                                 </a>
 
-                                <a href="https://www.laravelpodcast.com" class="w-1/2 text-gray-400">
+                                <a href="https://www.laravelpodcast.com" class="w-1/2 text-gray-400 hover:text-gray-200">
                                     <img src="{{ asset('images/podcast.png') }}" alt="Laravel Podcast" class="w-4 h-4 inline mr-2" />
                                     Laravel Podcast
                                 </a>
@@ -95,9 +95,9 @@
                 <span class="mb-5 lg:mb-0 lg:mr-5">© 2021 Laravel.io - All rights reserved.</span>
 
                 <div class="flex flex-wrap lg:block">
-                    <a href="https://laravel.io/terms" class="w-1/2 mb-4 lg:w-full lg:mb-0 lg:mr-8">Terms of service</a>
-                    <a href="https://laravel.io/privacy" class="w-1/2 mb-4 lg:w-full lg:mb-0 lg:mr-8">Privacy policy</a>
-                    <a href="https://github.com/sponsors/laravelio" class="w-1/2 lg:w-full lg:mb-0">Advertise</a>
+                    <a href="https://laravel.io/terms" class="w-1/2 mb-4 hover:text-gray-200 lg:w-full lg:mb-0 lg:mr-8">Terms of service</a>
+                    <a href="https://laravel.io/privacy" class="w-1/2 mb-4 hover:text-gray-200 lg:w-full lg:mb-0 lg:mr-8">Privacy policy</a>
+                    <a href="https://github.com/sponsors/laravelio" class="w-1/2 hover:text-gray-200 lg:w-full lg:mb-0">Advertise</a>
                 </div>
             </div>
         </div>
