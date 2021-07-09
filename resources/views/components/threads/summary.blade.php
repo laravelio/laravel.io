8403dae395be95561a21a4530d82eef7aa9aa4ec
Joe Dixon <hello@joedixon.co.uk>
2021-07-09 20:02:21 +0100

    Break long strings

--- resources/views/components/threads/summary.blade.php
+++ resources/views/components/threads/summary.blade.php
@@ -4,7 +4,7 @@
 
 <div class="h-full rounded shadow-lg p-5">
     <div class="h-full flex flex-col place-content-between">
-        <div>
+        <div class="break-all">
             <div class="flex items-center justify-between mb-2.5">
                 <div class="flex items-center">
                     <x-avatar :user="$thread->author()" class="w-8 h-8 rounded-full mr-2" />
