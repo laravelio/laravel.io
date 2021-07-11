c7241533b945e43811842e0f1a3e3a2724e26ced
Dries Vints <dries@vints.io>
2021-07-11 18:57:06 +0200

    Link mentioned users

--- app/Markdown/MarkdownServiceProvider.php
+++ app/Markdown/MarkdownServiceProvider.php
@@ -4,13 +4,29 @@ namespace App\Markdown;
 
 use Illuminate\Support\ServiceProvider;
 use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+use League\CommonMark\Extension\Mention\MentionExtension;
 
 class MarkdownServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->bind(Converter::class, function () {
-            return new LeagueConverter(new CommonMarkConverter(['html_input' => 'escape']));
+            $environment = Environment::createCommonMarkEnvironment();
+
+            $environment->addExtension(new MentionExtension);
+
+            $environment->mergeConfig([
+                'mentions' => [
+                    'username' => [
+                        'prefix' => '@',
+                        'pattern' => '[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)',
+                        'generator' => config('app.url').'/user/%s',
+                    ],
+                ],
+            ]);
+
+            return new LeagueConverter(new CommonMarkConverter(['html_input' => 'escape'], $environment));
         });
     }
 }
