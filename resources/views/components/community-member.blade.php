94468d26e44aec8cbee6be9fa386db2cf7b60026
Dries Vints <dries@vints.io>
2021-07-14 11:22:16 +0200

    Click-through on member

---
+++ resources/views/components/community-member.blade.php
@@ -0,0 +1,17 @@
+<div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
+    <x-stat-popout
+        x-cloak
+        x-show="active == '{{ $member->id }}'"
+        class="w-64 absolute -mt-40 -ml-20"
+        :user="$member"
+    />
+
+    <a href="{{ route('profile', $member->username) }}">
+        <x-avatar
+            :user="$member"
+            class="inset-0 w-14 h-14 md:w-20 md:h-20"
+            x-on:mouseover="active = {{ $member->id }}"
+            x-on:mouseout="active = false"
+        />
+    </a>
+</div>
--- resources/views/components/community-members.blade.php
+++ resources/views/components/community-members.blade.php
@@ -2,21 +2,7 @@
     @isset ($members[0])
         <div class="flex" style="margin-left: 134px;">
             @foreach ($members[0] as $member)
-                <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
-                    <x-stat-popout 
-                        x-cloak 
-                        x-show="active == '{{ $member->id }}'" 
-                        class="w-64 absolute -mt-40 -ml-20"
-                        :user="$member"
-                    />
-
-                    <x-avatar 
-                        :user="$member" 
-                        class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                        x-on:mouseover="active = {{ $member->id }}"
-                        x-on:mouseout="active = false"
-                    />
-                </div>
+                <x-community-member :member="$member" />
             @endforeach
         </div>
     @endisset
@@ -24,21 +10,7 @@
     @isset ($members[1])
         <div class="flex" style="margin-left: 71px;">
             @foreach ($members[1] as $member)
-                <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
-                    <x-stat-popout 
-                        x-cloak 
-                        x-show="active == '{{ $member->id }}'" 
-                        class="w-64 absolute -mt-40 -ml-20"
-                        :user="$member"
-                    />
-
-                    <x-avatar 
-                        :user="$member" 
-                        class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                        x-on:mouseover="active = {{ $member->id }}"
-                        x-on:mouseout="active = false"
-                    />
-                </div>
+                <x-community-member :member="$member" />
             @endforeach
         </div>
     @endisset
@@ -46,21 +18,7 @@
     @isset ($members[2])
         <div class="flex">
             @foreach ($members[2] as $member)
-                <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
-                    <x-stat-popout 
-                        x-cloak 
-                        x-show="active == '{{ $member->id }}'" 
-                        class="w-64 absolute -mt-40 -ml-20"
-                        :user="$member"
-                    />
-
-                    <x-avatar 
-                        :user="$member" 
-                        class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                        x-on:mouseover="active = {{ $member->id }}"
-                        x-on:mouseout="active = false"
-                    />
-                </div>
+                <x-community-member :member="$member" />
             @endforeach
         </div>
     @endisset
@@ -68,21 +26,7 @@
     @isset ($members[3])
         <div class="flex" style="margin-left: 71px;">
             @foreach ($members[3] as $member)
-                <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
-                    <x-stat-popout 
-                        x-cloak 
-                        x-show="active == '{{ $member->id }}'" 
-                        class="w-64 absolute -mt-40 -ml-20"
-                        :user="$member"
-                    />
-
-                    <x-avatar 
-                        :user="$member" 
-                        class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                        x-on:mouseover="active = {{ $member->id }}"
-                        x-on:mouseout="active = false"
-                    />
-                </div>
+                <x-community-member :member="$member" />
             @endforeach
         </div>
     @endisset
@@ -90,21 +34,7 @@
     @isset ($members[4])
         <div class="flex" style="margin-left: 134px;">
             @foreach ($members[4] as $member)
-                <div class="flex-shrink-0 w-14 h-14 md:w-20 md:h-20 mr-8 my-2 cursor-pointer">
-                    <x-stat-popout 
-                        x-cloak 
-                        x-show="active == '{{ $member->id }}'" 
-                        class="w-64 absolute -mt-40 -ml-20"
-                        :user="$member"
-                    />
-
-                    <x-avatar 
-                        :user="$member" 
-                        class="inset-0 w-14 h-14 md:w-20 md:h-20 rounded-full"
-                        x-on:mouseover="active = {{ $member->id }}"
-                        x-on:mouseout="active = false"
-                    />
-                </div>
+                <x-community-member :member="$member" />
             @endforeach
         </div>
     @endisset
