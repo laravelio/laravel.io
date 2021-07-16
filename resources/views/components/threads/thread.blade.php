1b5134099761bd4bde51ced575953a1c85e17a99
Joe Dixon <hello@joedixon.co.uk>
2021-07-16 13:13:35 +0100

    Add avatar links

--- resources/views/components/threads/thread.blade.php
+++ resources/views/components/threads/thread.blade.php
@@ -5,10 +5,10 @@
         <div class="flex flex-col lg:flex-row lg:justify-between lg:items-center px-6 py-4">
             <div>
                 <div class="flex flex-col lg:flex-row lg:items-center">
-                    <div class="flex">
-                        <x-avatar :user="$thread->author()" class="w-6 h-6 rounded-full mr-3" />
+                    <div>
+                        <a href="{{ route('profile', $thread->author()->username()) }}" class="flex items-center hover:underline">
+                            <x-avatar :user="$thread->author()" class="w-6 h-6 rounded-full mr-3" />
 
-                        <a href="{{ route('profile', $thread->author()->username()) }}" class="hover:underline">
                             <span class="text-gray-900 mr-5">{{ $thread->author()->name() }}</span>
                         </a>
                     </div>
--- resources/views/components/users/profile-block.blade.php
+++ resources/views/components/users/profile-block.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col items-center justify-center bg-white rounded-b-md pb-8">
         <x-avatar :user="$user" class="w-32 h-32 rounded-full -mt-16 mb-6" />
 
-        <a href="{{ route('profile', $user->username()) }}" class="text-xl font-medium mb-2">
+        <a href="{{ route('profile', $user->username()) }}" class="text-xl font-medium mb-2 hover:underline">
             {{ $user->name() }}
         </a>
 
@@ -15,14 +15,14 @@
         </span>
 
         @if ($user->githubUsername())
-            <a href="https://github.com/{{ $user->githubUsername() }}" class="flex items-center gap-x-2 mb-3 font-medium">
+            <a href="https://github.com/{{ $user->githubUsername() }}" class="flex items-center gap-x-2 mb-3 font-medium hover:underline">
                 <x-icon-github class="w-6 h-6" />
                 {{ '@' . $user->githubUsername() }}
             </a>
         @endif
 
         @if ($user->hasTwitterAccount())
-            <a href="https://twitter.com/{{ $user->twitter() }}" class="flex items-center gap-x-2 text-twitter font-medium">
+            <a href="https://twitter.com/{{ $user->twitter() }}" class="flex items-center gap-x-2 text-twitter font-medium hover:underline">
                 <x-icon-twitter class="w-6 h-6" />
                 {{ '@' . $user->twitter() }}
             </a>
