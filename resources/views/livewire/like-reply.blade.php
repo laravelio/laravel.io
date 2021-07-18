a83197ebc2c18718f4a1e8fc13f75945c14a86aa
Dries Vints <dries.vints@gmail.com>
2021-07-18 15:07:23 +0200

    Update like-reply.blade.php

--- resources/views/livewire/like-reply.blade.php
+++ resources/views/livewire/like-reply.blade.php
@@ -2,6 +2,7 @@
     @if (Auth::guest())
         <div class="flex items-center gap-x-2">
             <x-heroicon-o-thumb-up class="w-6 h-6" />
+            
             <span class="font-medium">
                 {{ count($this->reply->likes()) }}
             </span>
@@ -9,9 +10,11 @@
     @else 
         <button type="button" wire:click="toggleLike" class="flex items-center gap-x-2 text-lio-500">
             <x-heroicon-o-thumb-up class="w-6 h-6" />
+            
             <span class="font-medium">
                 {{ count($this->reply->likes()) }}
             </span>
+            
             @if ($this->reply->isLikedBy(Auth::user()))
                 <span class="text-gray-400 text-sm italic ml-1">You liked this reply</span>
             @endif
