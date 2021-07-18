4c3c3599026efe57ec912709d072590962232e74
Dries Vints <dries.vints@gmail.com>
2021-07-18 15:06:43 +0200

    Update thread.blade.php

--- resources/views/components/threads/thread.blade.php
+++ resources/views/components/threads/thread.blade.php
@@ -8,7 +8,6 @@
                     <div>
                         <a href="{{ route('profile', $thread->author()->username()) }}" class="flex items-center hover:underline">
                             <x-avatar :user="$thread->author()" class="w-6 h-6 rounded-full mr-3" />
-
                             <span class="text-gray-900 mr-5">{{ $thread->author()->name() }}</span>
                         </a>
                     </div>
