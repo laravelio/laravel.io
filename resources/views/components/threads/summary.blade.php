84a29e8215309b009b18d0c6f1b22e5e08ac28be
Dries Vints <dries@vints.io>
2021-07-19 20:45:33 +0200

    fix links

--- resources/views/components/threads/summary.blade.php
+++ resources/views/components/threads/summary.blade.php
@@ -9,6 +9,8 @@
                 <div class="flex items-center">
                     <a href="{{ route('profile', $thread->author()->username()) }}">
                         <x-avatar :user="$thread->author()" class="w-8 h-8 rounded-full mr-2" />
+                    </a>
+                    <a href="{{ route('profile', $thread->author()->username()) }}">
                         <span class="font-heading text-sm text-black">{{ $thread->author()->name() }}</span>
                     </a>
                 </div>
