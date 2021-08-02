6891487e8a59cac191ec25b3be36e86ee4d85c64
Dries Vints <dries@vints.io>
2021-08-02 21:22:51 +0200

    Fix migration

--- database/migrations/2021_07_31_183222_unique_github_ids.php
+++ database/migrations/2021_07_31_183222_unique_github_ids.php
@@ -2,12 +2,25 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up()
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('github_id')->nullable()->change();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('github_username')->nullable()->change();
+        });
+
+        if (! app()->runningUnitTests()) {
+            DB::table('users')->where('github_id', '')->update(['github_id' => null]);
+            DB::table('users')->where('github_username', '')->update(['github_username' => null]);
+        }
+
         Schema::table('users', function (Blueprint $table) {
             $table->unique('github_id');
         });
