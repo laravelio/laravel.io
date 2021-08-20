ee8da2a17b1f0280c4f25a379f2eb725a4abef7c
Joe Dixon <hello@joedixon.co.uk>
2021-08-20 16:05:22 +0100

    Remove comment

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,11 +10,6 @@ class ProfileController extends Controller
     public function show(User $user, Request $request)
     {
         abort_unless($user = $user->exists ? $user : $request->user(), 404);
-        // $user = $user->exists ? $user : $request->user();
-
-        // if (! $user) {
-        //     abort(404);
-        // }
 
         return view('users.profile', compact('user'));
     }
