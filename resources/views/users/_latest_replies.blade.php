a951f4a5e9d80b424c20007491510a3377c40745
Joe Dixon <hello@joedixon.co.uk>
2021-09-01 20:14:25 +0100

    Add profile reply component

--- resources/views/users/_latest_replies.blade.php
+++ resources/views/users/_latest_replies.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col gap-y-6">
     @forelse ($user->latestReplies(5) as $reply)
-        <x-threads.reply :thread="$reply->replyAble()" :reply="$reply" />
+        <x-users.reply :thread="$reply->replyAble()" :reply="$reply" />
     @empty
         <p class="text-gray-600 text-base">
             {{ $user->username() }} has not posted any replies yet
