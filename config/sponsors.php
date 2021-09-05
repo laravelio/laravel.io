496d0040b8965a84b1f8ee42524da01129899cef
faissaloux <fwahabali@gmail.com>
2021-09-05 01:46:16 +0100

    group sponsors in config file

---
+++ config/sponsors.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    "Beyond Code" => [
+        "url" => "https://beyondco.de",
+        "logo" => "images/sponsors/beyondcode.png"
+    ],
+    "DevSquad" => [
+        "url" => "https://devsquad.com",
+        "logo" => "images/sponsors/devsquad.png"
+    ],
+    "Fathom" => [
+        "url" => "https://usefathom.com/ref/7A8QGC",
+        "logo" => "images/sponsors/fathom.png"
+    ],
+    "Forge" => [
+        "url" => "https://forge.laravel.com",
+        "logo" => "images/sponsors/forge.png"
+    ],
+    "Envoyer" => [
+        "url" => "https://envoyer.io",
+        "logo" => "images/sponsors/envoyer.png"
+    ],
+    "Blackfire.io" => [
+        "url" => "https://blackfire.io",
+        "logo" => "images/sponsors/blackfire-io.png"
+    ],
+    "Akaunting" => [
+        "url" => "https://akaunting.com/developers?utm_source=Laravelio&utm_medium=Banner&utm_campaign=Developers",
+        "logo" => "images/sponsors/akaunting.png"
+    ],
+    "LaraJobs" => [
+        "url" => "https://larajobs.com",
+        "logo" => "images/sponsors/larajobs.svg"
+    ],
+    "Scout APM" => [
+        "url" => "https://ter.li/vj4bxb",
+        "logo" => "images/sponsors/scout-apm.jpg",
+        "offset" => "lg:col-start-2"
+    ],
+    "Cloudways" => [
+        "url" => "https://www.cloudways.com/en/?id=972670",
+        "logo" => "images/sponsors/cloudways.png"
+    ]
+];
--- resources/views/layouts/_sponsors.blade.php
+++ resources/views/layouts/_sponsors.blade.php
@@ -5,45 +5,12 @@
         </p>
 
         <div class="mt-6 grid grid-cols-2 gap-y-8 lg:grid-cols-4">
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://beyondco.de" logo="{{ asset('images/sponsors/beyondcode.png') }}" company="Beyond Code" />
-            </div>
+            @foreach (config('sponsors') as $company => $sponsor)
+                <div class="col-span-2 flex justify-center lg:col-span-1 {{ $sponsor['offset'] ?? '' }}">
+                    <x-sponsor-logo url="{{ $sponsor['url'] }}" logo="{{ asset($sponsor['logo']) }}" company="{{ $company }}" />
+                </div>
+            @endforeach
 
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://devsquad.com" logo="{{ asset('images/sponsors/devsquad.png') }}" company="DevSquad" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://usefathom.com/ref/7A8QGC" logo="{{ asset('images/sponsors/fathom.png') }}" company="Fathom" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://forge.laravel.com/" logo="{{ asset('images/sponsors/forge.png') }}" company="Forge" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://envoyer.io/" logo="{{ asset('images/sponsors/envoyer.png') }}" company="Envoyer" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://blackfire.io/" logo="{{ asset('images/sponsors/blackfire-io.png') }}" company="Blackfire.io" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://akaunting.com/developers?utm_source=Laravelio&utm_medium=Banner&utm_campaign=Developers" logo="{{ asset('images/sponsors/akaunting.png') }}" company="Akaunting" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://larajobs.com" logo="{{ asset('images/sponsors/larajobs.svg') }}" company="LaraJobs" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1 lg:col-start-2">
-                <x-sponsor-logo url="https://ter.li/vj4bxb" logo="{{ asset('images/sponsors/scout-apm.jpg') }}" company="Scout APM" />
-            </div>
-
-            <div class="col-span-2 flex justify-center lg:col-span-1">
-                <x-sponsor-logo url="https://www.cloudways.com/en/?id=972670" logo="{{ asset('images/sponsors/cloudways.png') }}" company="Cloudways" />
-            </div>
         </div>
 
         <x-ads.cta primary class="mt-8 md:mt-12">
