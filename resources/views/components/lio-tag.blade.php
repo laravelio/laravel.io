bf3134471ec8843415a9d6d04ffc46a3ec4adf42
Dries Vints <dries@vints.io>
2021-09-29 22:14:13 +0200

    Move label

--- resources/views/components/lio-tag.blade.php
+++
@@ -1,3 +0,0 @@
-<span class="text-sm text-lio-500 border border-lio-200 rounded py-1.5 px-3 leading-none">
-    {{ $slot }}
-</span>
--- resources/views/components/threads/reply-menu.blade.php
+++ resources/views/components/threads/reply-menu.blade.php
@@ -1,15 +1,16 @@
 @props(['thread', 'reply'])
 
 <div class="flex items-center lg:gap-x-3">
-
-    @if($reply->author()->isModerator() || $reply->author()->isAdmin())
-        <x-lio-tag>moderator</x-lio-tag>
+    @if ($reply->author()->isModerator() || $reply->author()->isAdmin())
+        <span class="text-sm text-lio-500 border border-lio-200 rounded py-1.5 px-3 leading-none">
+            moderator
+        </span>
     @endif
-    
+
     @can(App\Policies\ThreadPolicy::UPDATE, $thread)
-        @if ($thread->isSolutionReply($reply))      
-            <button 
-                class="flex items-center gap-x-2 font-medium text-lio-500 hover:text-gray-300" 
+        @if ($thread->isSolutionReply($reply))
+            <button
+                class="flex items-center gap-x-2 font-medium text-lio-500 hover:text-gray-300"
                 @click="$dispatch('open-modal', 'unmark-solution-{{ $thread->id }}')"
             >
                 <x-heroicon-o-badge-check class="w-6 h-6" />
@@ -23,8 +24,8 @@
                 'body' => '<p>Confirm to unmark this reply as the solution for <strong>"'.e($thread->subject()).'"</strong>.</p>',
             ])
         @else
-            <button 
-                class="flex items-center gap-x-2 font-medium text-gray-300 hover:text-lio-500" 
+            <button
+                class="flex items-center gap-x-2 font-medium text-gray-300 hover:text-lio-500"
                 @click="$dispatch('open-modal', 'mark-solution-{{ $reply->id }}')"
             >
                 <x-heroicon-o-badge-check class="w-6 h-6" />
@@ -49,19 +50,19 @@
 
     @can(App\Policies\ReplyPolicy::UPDATE, $reply)
         <div class="relative -mr-3" x-data="{ open: false }" @click.away="open = false">
-            
-            <button 
-                class="p-2 rounded hover:bg-gray-100" 
-                @click="open = !open"    
+
+            <button
+                class="p-2 rounded hover:bg-gray-100"
+                @click="open = !open"
             >
                 <x-heroicon-o-dots-horizontal class="w-6 h-6" />
             </button>
 
-            <div 
-                x-cloak 
-                x-show="open" 
+            <div
+                x-cloak
+                x-show="open"
                 class="absolute top-12 right-1 flex flex-col bg-white rounded shadow w-48"
-            >                
+            >
 
                 <a class="flex gap-x-2 p-3 rounded hover:bg-gray-100" href="{{ route('replies.edit', $reply->id()) }}">
                     <x-heroicon-o-pencil class="w-6 h-6"/>
@@ -71,7 +72,7 @@
                 <button class="flex gap-x-2 p-3 rounded hover:bg-gray-100" @click="$dispatch('open-modal', 'delete-reply-{{ $reply->id }}')">
                     <x-heroicon-o-trash class="w-6 h-6 text-red-500"/>
                     Delete
-                </button>          
+                </button>
             </div>
         </div>
 
