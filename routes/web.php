4059e3572faf3043408730666fee933446ef429b
Joe Dixon <hello@joedixon.co.uk>
2021-10-11 18:32:44 +0100

    Redirect from old dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,27 @@
 <?php
 
-use App\Http\Controllers\Admin\ArticlesController as AdminArticlesController;
-use App\Http\Controllers\Admin\UsersController;
-use App\Http\Controllers\Articles\ArticlesController;
-use App\Http\Controllers\Articles\AuthoredArticles;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\GithubController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\ResetPasswordController;
-use App\Http\Controllers\Auth\VerificationController;
-use App\Http\Controllers\Forum\TagsController;
-use App\Http\Controllers\Forum\ThreadsController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ReplyController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReplyAbleController;
-use App\Http\Controllers\ReplyController;
-use App\Http\Controllers\Settings\PasswordController;
-use App\Http\Controllers\Settings\ProfileController as ProfileSettingsController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Forum\TagsController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Auth\GithubController;
 use App\Http\Controllers\SocialImageController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\SubscriptionController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Forum\ThreadsController;
+use App\Http\Controllers\Articles\AuthoredArticles;
+use App\Http\Controllers\Articles\ArticlesController;
+use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Controllers\Settings\PasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Admin\ArticlesController as AdminArticlesController;
+use App\Http\Controllers\Settings\ProfileController as ProfileSettingsController;
 
 Route::feeds();
 
@@ -60,6 +60,9 @@ Route::namespace('Auth')->group(function () {
     Route::get('login/github', [GithubController::class, 'redirectToProvider'])->name('login.github');
     Route::get('auth/github', [GithubController::class, 'handleProviderCallback']);
 });
+
+// Redirect the old dashboard route to /user
+Route::redirect('/dashboard', '/user');
 
 // Users
 Route::get('user/{username?}', [ProfileController::class, 'show'])->name('profile');
