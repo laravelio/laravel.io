4542120576f4fbb7e78dbf7fde508ac102bfdd28
Dries Vints <dries@vints.io>
2021-10-19 21:22:49 +0200

    Update banners

--- config/lio.php
+++ config/lio.php
@@ -5,7 +5,7 @@ return [
     'ads' => [
         ['url' => 'https://devsquad.com', 'image' => 'devsquad', 'alt' => 'Devsquad', 'goal' => 'MMCR7T3S'],
         ['url' => 'https://larajobs.com', 'image' => 'larajobs', 'alt' => 'LaraJobs', 'goal' => '9C3CAYKR'],
-        ['url' => 'https://fullstackeurope.com/2021?utm_source=Laravel.io&utm_campaign=fseu21&utm_medium=advertisement', 'image' => 'fseu', 'alt' => 'Full Stack Europe', 'goal' => 'SRIK2PEE'],
+        ['url' => 'https://fullstackeurope.com/2022?utm_source=Laravel.io&utm_campaign=fseu22&utm_medium=advertisement', 'image' => 'fseu', 'alt' => 'Full Stack Europe', 'goal' => 'SRIK2PEE'],
         [
             'url' => 'https://www.cloudways.com/en/php-hosting.php?id=972670',
             'long-url' => 'https://www.cloudways.com/en/laravel-hosting?id=972670',
