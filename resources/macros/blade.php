abea0dbf941b3da3152a2141890538fdcab02ebc
Joe Dixon <hello@joedixon.co.uk>
2021-11-02 21:19:31 +0000

    Fix error styles

--- resources/macros/blade.php
+++ resources/macros/blade.php
@@ -7,7 +7,7 @@ Blade::directive('md', function ($expression) {
 });
 
 Blade::directive('error', function ($expression) {
-    return "<?php echo \$errors->first($expression, '<span class=\"help-block\">:message</span>'); ?>";
+    return "<?php echo \$errors->first($expression, '<span class=\"block text-sm text-red-500 mt-2\">:message</span>'); ?>";
 });
 
 Blade::directive('formGroup', function ($expression) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     purge: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
+        './resources/macros/blade.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
     theme: {
