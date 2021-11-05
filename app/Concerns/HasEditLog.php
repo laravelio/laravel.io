7c3b2336880b468291ce90eda37c744d018f03c3
tomhatzer <me@tomhatzer.at>
2021-11-05 22:35:25 +0100

    Fix styling

--- app/Concerns/HasEditLog.php
+++ app/Concerns/HasEditLog.php
@@ -6,24 +6,21 @@ use App\Models\Edit;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
-use function constant;
-use function json_encode;
-use function sprintf;
 
 trait HasEditLog
 {
     public static function bootHasEditLog()
     {
-        static::updating(function($model) {
+        static::updating(function ($model) {
             Edit::create([
                 'author_id' => auth()->id(),
                 'editable_id' => $model->id,
-                'editable_type' => constant($model::class . '::TABLE') ?? $model::class,
+                'editable_type' => constant($model::class.'::TABLE') ?? $model::class,
                 'edited_at' => now(),
             ]);
 
             $cacheKey = sprintf('%s-%s', Str::slug($model::class), $model->id);
-            if(Cache::has($cacheKey)) {
+            if (Cache::has($cacheKey)) {
                 Cache::forget($cacheKey);
             }
         });
@@ -38,7 +35,7 @@ trait HasEditLog
     {
         $cacheKey = sprintf('%s-%s', Str::slug($this::class), $this->id);
 
-        return Cache::rememberForever($cacheKey, function() {
+        return Cache::rememberForever($cacheKey, function () {
             return $this->edits()->latest('edited_at')->first();
         });
     }
