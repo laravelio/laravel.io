aa58783b5d48990e8e39ec989eb701842a0f9131
faissaloux <fwahabali@gmail.com>
2021-11-09 00:33:56 +0100

    fix article control buttons

--- resources/views/components/articles/actions.blade.php
+++ resources/views/components/articles/actions.blade.php
@@ -1,38 +1,64 @@
 <div class="flex flex-col gap-x-4 gap-y-3 lg:flex-row lg:justify-between">
-    <div class="flex flex-col gap-x-4 gap-y-3 lg:flex-row">
+    <div
+        class="flex flex-col gap-x-4 gap-y-3 lg:flex-row"
+        x-data="{ hovered:false }"
+    >
         @if (Auth::check() && $article->isAuthoredBy(Auth::user()))
-            <x-buttons.secondary-button href="{{ route('articles.edit', $article->slug()) }}" class="w-full">
+            <x-buttons.secondary-button
+                href="{{ route('articles.edit', $article->slug()) }}"
+                @mouseover="hovered = 'edit'"
+                @mouseleave="hovered = false"
+                class="w-full"
+            >
                 <span class="flex items-center gap-x-2">
                     <x-heroicon-o-pencil class="w-5 h-5" title="Edit" />
-                    Edit article
+                    <span x-show="hovered === 'edit'">Edit article</span>
                 </span>
             </x-buttons.primary-menu-button>
         @endif
 
         @if ($article->isNotPublished() && $article->isAwaitingApproval())
             @can(App\Policies\ArticlePolicy::APPROVE, $article)
-                <x-buttons.secondary-button tag="button" @click.prevent="activeModal = 'approveArticle'" class="w-full">
+                <x-buttons.secondary-button
+                    tag="button"
+                    @click.prevent="activeModal = 'approveArticle'"
+                    @mouseover="hovered = 'publish'"
+                    @mouseleave="hovered = false"
+                    class="w-full"
+                >
                     <span class="flex items-center gap-x-2">
                         <x-heroicon-o-eye class="w-5 h-5" title="Publish"/>
-                        Publish article
+                        <span x-show="hovered === 'publish'">Publish article</span>
                     </span>
                 </x-buttons.secondary-button>
             @endcan
 
             @can(App\Policies\ArticlePolicy::DECLINE, $article)
-                <x-buttons.secondary-button tag="button" @click.prevent="activeModal = 'declineArticle'" class="w-full">
+                <x-buttons.secondary-button
+                    tag="button"
+                    @click.prevent="activeModal = 'declineArticle'"
+                    @mouseover="hovered = 'decline'"
+                    @mouseleave="hovered = false"
+                    class="w-full"
+                >
                     <span class="flex items-center gap-x-2">
                         <x-heroicon-o-eye-off class="w-5 h-5" title="Decline"/>
-                        Decline article
+                        <span x-show="hovered === 'decline'">Decline article</span>
                     </span>
                 </x-buttons.secondary-button>
             @endcan
         @else
             @can(App\Policies\ArticlePolicy::DISAPPROVE, $article)
-                <x-buttons.secondary-button tag="button" @click.prevent="activeModal = 'disapproveArticle'" class="w-full">
+                <x-buttons.secondary-button
+                    tag="button"
+                    @click.prevent="activeModal = 'disapproveArticle'"
+                    @mouseover="hovered = 'unpublish'"
+                    @mouseleave="hovered = false"
+                    class="w-full"
+                >
                     <span class="flex items-center gap-x-2">
                         <x-heroicon-o-eye-off class="w-5 h-5" title="Unpublish"/>
-                        Unpublish article
+                        <span x-show="hovered === 'unpublish'">Unpublish article</span>
                     </span>
                 </x-buttons.secondary-button>
             @endcan
