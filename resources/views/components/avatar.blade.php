4c498bb1e3a3c319844f521923814457e40b8945
Joel Butcher <joel@clicktech.com>
2021-11-19 14:48:04 +0000

    added new default avatar

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -7,13 +7,23 @@
     <a href="{{ route('profile', $user->username()) }}">
 @endunless
 
-<x-buk-avatar
-    :search="$user->githubUsername()"
-    provider="github"
-    :fallback="asset('/images/user.svg')"
-    :alt="$user->name()"
-    {{ $attributes->merge(['class' => 'rounded-full text-gray-500']) }}
-/>
+@if ($user->githubUsername())
+    <x-buk-avatar
+        :search="$user->githubUsername()"
+        provider="github"
+        :fallback="asset('/images/user.svg')"
+        :alt="$user->name()"
+        {{ $attributes->merge(['class' => 'rounded-full text-gray-500']) }}
+    />
+@else
+    <div {{ $attributes->merge(['class' => 'bg-gray-100 rounded-full text-gray-500 relative flex items-center justify-center']) }}>
+        <img
+            src="{{ asset('images/laravel.png') }}"
+            alt="{{ $user->name() }}"
+            class="filter grayscale"
+        />
+    </div>
+@endif
 
 @unless ($unlinked)
     </a>
