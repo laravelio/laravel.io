3600d59f780bf5cf26ed32499e347549a195e0f7
Dries Vints <dries@vints.io>
2021-11-21 12:00:26 +0100

    wip

---
+++ tests/Integration/Mail/NewReplyEmailTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Jobs\BanUser;
+use App\Mail\NewReplyEmail;
+use App\Models\Reply;
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+uses(TestCase::class);
+uses(DatabaseMigrations::class);
+
+it('contains a note about solutions when the receiver is the thread author', function () {
+    $reply = Reply::factory()->create();
+    $thread = $reply->replyAble();
+    $subscription = Subscription::factory()->create(['subscriptionable_id' => $thread->id]);
+
+    $email = (new NewReplyEmail($reply, $subscription, $thread->author()))->render();
+
+    expect($email)
+        ->toContain('Please make sure to mark the correct reply as the solution when your question gets answered.');
+});
+
+it('misses the note about solutions when the receiver is not the thread author', function () {
+    $reply = Reply::factory()->create();
+    $thread = $reply->replyAble();
+    $user = User::factory()->create();
+    $subscription = Subscription::factory()->create(['subscriptionable_id' => $thread->id]);
+
+    $email = (new NewReplyEmail($reply, $subscription, $user))->render();
+
+    expect($email)->not
+        ->toContain('Please make sure to mark the correct reply as the solution when your question gets answered.');
+});
