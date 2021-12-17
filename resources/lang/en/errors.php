d67304ed53d8da4f9e63b574950a043c528d52f9
marvelefe <marvelcamoarmy@gmail.com>
2021-12-18 00:59:31 +0100

    styleci fixes

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -7,6 +7,6 @@ return [
     'fields' => 'Something went wrong. Please review the fields below.',
     'github_invalid_state' => 'The request timed out. Please try again.',
     'github_account_too_young' => 'Your Github account needs to be older than 2 weeks in order to register.',
-    'github_account_exists' => 'We already found a user with the given GitHub account (https://github/'.$githubUsername ?? ''.'). Would you like to login instead?'
-    
+    'github_account_exists' => 'We already found a user with the given GitHub account (https://github/'.$githubUsername ?? ''.'). Would you like to login instead?',
+
 ];
