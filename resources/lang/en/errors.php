d2c9ca98203b2a02c37e56b09abc69f2cc33c1ab
marvelefe <marvelcamoarmy@gmail.com>
2021-12-18 00:40:57 +0100

    fixed failing test

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -1,9 +1,10 @@
-<?php
+<?php 
+$githubUsername = is_array(session('githubData')) ? session('githubData')['username'] : '';
 
 return [
     'banned' => 'This account is banned.',
     'fields' => 'Something went wrong. Please review the fields below.',
     'github_invalid_state' => 'The request timed out. Please try again.',
     'github_account_too_young' => 'Your Github account needs to be older than 2 weeks in order to register.',
-    'github_account_exists' => 'We already found a user with the given GitHub account (https://github/'.session('githubData')['username'].'). Would you like to login instead?'
+    'github_account_exists' => 'We already found a user with the given GitHub account (https://github/'.$githubUsername ?? ''.'). Would you like to login instead?'
 ];
