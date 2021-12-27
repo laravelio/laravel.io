4cfcbd39480eb1c5ccc1bc9ac4a59e09401b4444
Luke Downing <lukeraymonddowning@gmail.com>
2021-12-27 13:25:55 +0000

    Adds tests for api token jobs.

---
+++ tests/Integration/Jobs/CreateApiTokenTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Jobs\CreateApiToken;
+use Database\Factories\UserFactory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+uses(TestCase::class);
+uses(DatabaseMigrations::class);
+
+it('creates an API token for the given user', function () {
+    $user = $this->createUser();
+
+    $this->dispatch(new CreateApiToken($user, 'Foo Bar'));
+
+    expect($user->refresh()->tokens)
+        ->toHaveCount(1)
+        ->first()->name->toBe('Foo Bar');
+});
