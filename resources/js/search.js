d0281f55c38ad75bedb8e9aeb75665d686155eaf
Joe Dixon <hello@joedixon.co.uk>
2021-12-28 11:02:41 +0000

    Refactor search to object

--- resources/js/search.js
+++ resources/js/search.js
@@ -2,29 +2,54 @@ import algoliasearch from 'algoliasearch/lite';
 
 const client = algoliasearch(process.env.MIX_ALGOLIA_APP_ID, process.env.MIX_ALGOLIA_SECRET);
 
-window.search = (event) => {
-    // If the input is empty, return no results.
-    if (event.target.value.length === 0) {
-        return Promise.resolve({ hits: [] });
-    }
+window.searchConfig = () => {
+    return {
+        show: false,
+        threads: {
+            total: 0,
+            formattedTotal: function () {
+                return `${this.total} ${this.total === 1 ? 'Result' : 'Results'}`
+            },
+            threads: [],
+        },
+        articles: {
+            total: 0,
+            formattedTotal: function () {
+                return `${this.total} ${this.total === 1 ? 'Result' : 'Results'}`
+            },
+            articles: [],
+        },
+        search: async function (query) {
+            // If the input is empty, return no results.
+            if (query.length === 0) {
+                return Promise.resolve({ hits: [] });
+            }
 
-    // Perform the search using the provided input.
-    return client.multipleQueries([
-        {
-            indexName: process.env.MIX_ALGOLIA_THREADS_INDEX,
-            query: event.target.value,
-            params: {
-                hitsPerPage: 5,
-                attributesToSnippet: ['body:10'],
-            },
-        },
-        {
-            indexName: process.env.MIX_ALGOLIA_ARTICLES_INDEX,
-            query: event.target.value,
-            params: {
-                hitsPerPage: 5,
-                attributesToSnippet: ['body:10'],
-            },
-        },
-    ]);
-};
+            // Perform the search using the provided input.
+            const { results } = await client.multipleQueries([
+                {
+                    indexName: process.env.MIX_ALGOLIA_THREADS_INDEX,
+                    query: query,
+                    params: {
+                        hitsPerPage: 5,
+                        attributesToSnippet: ['body:10'],
+                    },
+                },
+                {
+                    indexName: process.env.MIX_ALGOLIA_ARTICLES_INDEX,
+                    query: query,
+                    params: {
+                        hitsPerPage: 5,
+                        attributesToSnippet: ['body:10'],
+                    },
+                },
+            ]);
+
+            this.show = true;
+            this.threads.total = results[0].nbHits;
+            this.threads.threads = results[0].hits;
+            this.articles.total = results[1].nbHits;
+            this.articles.articles = results[1].hits;
+        }
+    };
+}
