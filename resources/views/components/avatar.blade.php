9eb234c2264eb632684187d702d725c169d539ee
Joel Butcher <joelbutcher@users.noreply.github.com>
2022-01-08 15:18:31 +0000

    Add a new default avatar using a greyscale Laravel logo (#759)
    * added new default avatar
    
    * added tests
    
    * use laravelio logo
    
    * use lighter gray
    
    * fix tests
    
    * wip
    
    * wip
    
    * wip
    
    Co-authored-by: Joel Butcher <joel@clicktech.com>
    Co-authored-by: Dries Vints <dries@vints.io>

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -7,13 +7,21 @@
     <a href="{{ route('profile', $user->username()) }}">
 @endunless
 
-<x-buk-avatar
-    :search="$user->githubUsername()"
-    provider="github"
-    :fallback="asset('/images/user.svg')"
-    :alt="$user->name()"
-    {{ $attributes->merge(['class' => 'rounded-full text-gray-500']) }}
-/>
+@if ($user->githubUsername())
+    <x-buk-avatar
+        :search="$user->githubUsername()"
+        provider="github"
+        :fallback="asset('/images/laravelio-icon-gray.svg')"
+        :alt="$user->name()"
+        {{ $attributes->merge(['class' => 'bg-gray-50 rounded-full']) }}
+    />
+@else
+    <img
+        src="{{ asset('images/laravelio-icon-gray.svg') }}"
+        alt="{{ $user->name() }}"
+        {{ $attributes->merge(['class' => 'bg-gray-50 rounded-full']) }}
+    />
+@endif
 
 @unless ($unlinked)
     </a>
