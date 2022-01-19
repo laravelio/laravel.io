b337c64fe6612b4b5ac2281d487110808edaa19f
Faissal Wahabali <fwahabali@gmail.com>
2022-01-19 11:22:07 +0100

    persist article declined banner (#808)

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,6 +6,14 @@
 
 @section('content')
     <article class="bg-white">
+        @if ($article->isDeclined() && (Auth::user()->isAdmin() || Auth::user()->isModerator()))
+            <div class="w-full text-white bg-lio-500 p-4">
+                <div class="text-center container mx-auto px-4">
+                    {{ __('admin.articles.declined') }}
+                </div>
+            </div>
+        @endif
+
         <div class="w-full bg-center bg-cover bg-gray-900" style="background-image: linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url({{ $article->heroImage(2000, 384) }});">
             <div class="container mx-auto">
                 <div class="px-4 lg:px-0 lg:mx-48">
