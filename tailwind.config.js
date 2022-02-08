236f12443f5801737d82724e8e2ade8343026e53
wlsquid <wyndhammorland@gmail.com>
2022-02-08 16:24:27 +1000

    API token moved to component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/macros/blade.php',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './resources/lang/en/settings.php'
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php'
     ],
     theme: {
         container: {
