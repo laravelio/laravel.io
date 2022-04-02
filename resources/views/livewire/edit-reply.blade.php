e450018bac7e084793de62f9c94c0f6bcf680630
Joe Dixon <hello@joedixon.co.uk>
2022-04-02 15:57:16 +0100

    Fix formatting

--- resources/views/livewire/edit-reply.blade.php
+++ resources/views/livewire/edit-reply.blade.php
@@ -4,10 +4,8 @@
         x-data
         x-init="$nextTick(function () { highlightCode($el); }); $watch('edit', function () { highlightCode($el); });"
         class="prose prose-lio max-w-none p-6 break-words"
+        x-html="{{ json_encode(replace_links(md_to_html($reply->body()))) }}"
     >
-        <x-buk-markdown>
-            {!! replace_links(md_to_html($reply->body())) !!}
-        </x-buk-markdown>
     </div>
 
     @can(App\Policies\ReplyPolicy::UPDATE, $reply)
