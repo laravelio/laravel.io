eae62121264ee498af9fb7a5ef4bec3900514915
Joe Dixon <joedixon@users.noreply.github.com>
2022-04-14 10:01:02 +0100

    Order RSS article feed by most recent (#858)
    * Remove pinned status from recent scope
    
    * Order RSS feed by most recent

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -270,8 +270,7 @@ final class Article extends Model implements Feedable
 
     public function scopeRecent(Builder $query): Builder
     {
-        return $query->orderBy('is_pinned', 'desc')
-            ->orderBy('submitted_at', 'desc');
+        return $query->orderBy('submitted_at', 'desc');
     }
 
     public function scopePopular(Builder $query): Builder
@@ -328,6 +327,7 @@ final class Article extends Model implements Feedable
     public static function getFeedItems(): Collection
     {
         return self::published()
+            ->recent()
             ->paginate(self::FEED_PAGE_SIZE)
             ->getCollection();
     }
