81db03bf10815e0e5f96931d43af60246161fe7c
Youri Wijnands <youri@wijnands.me>
2022-05-10 20:24:23 +0200

    Make UUID's non-nullable

---
+++ database/migrations/2022_05_10_180922_make_uuids_non_nullable.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public const TABLES = [
+        'articles',
+        'threads',
+        'replies',
+    ];
+
+    public function up()
+    {
+        foreach (static::TABLES as $tableName) {
+            Schema::table($tableName, function (Blueprint $table) use ($tableName) {
+                $table->dropIndex($tableName . '_uuid_index');
+
+                $table->uuid('uuid')
+                    ->nullable(false)
+                    ->unique()
+                    ->change();
+            });
+        }
+    }
+};
