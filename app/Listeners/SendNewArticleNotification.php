6cae6a107afcbba8722980ec9bc967dd97d57478
Dries Vints <dries@vints.io>
2022-05-16 16:19:19 +0200

    wip

--- app/Listeners/SendNewArticleNotification.php
+++ app/Listeners/SendNewArticleNotification.php
@@ -8,9 +8,8 @@ use Illuminate\Notifications\AnonymousNotifiable;
 
 final class SendNewArticleNotification
 {
-    public function __construct(
-        private AnonymousNotifiable $notifiable
-    ) {
+    public function __construct(private AnonymousNotifiable $notifiable)
+    {
     }
 
     public function handle(ArticleWasSubmittedForApproval $event): void
