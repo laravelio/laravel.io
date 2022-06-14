3ddf777c5a3b9efeab36210c84180f031028a72c
sawirricardo <sawir.ricardo@gmail.com>
2022-06-14 13:55:49 +0700

    store state of who mark which thread as spam

---
+++ database/migrations/2022_06_14_054821_create_spam_markers_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('spam_markers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->unsignedInteger('thread_id')->nullable();
+            $table->foreign('thread_id')->references('id')->on('threads')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('spam_markers');
+    }
+};
