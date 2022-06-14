fbe19ac29124ee6bb99a36e1e57eb281540f61f6
sawirricardo <sawir.ricardo@gmail.com>
2022-06-14 13:57:04 +0700

    Create MarkThreadAsSpam.php

---
+++ app/Jobs/MarkThreadAsSpam.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Thread;
+use App\Models\User;
+use App\Notifications\ThreadMarkedAsSpamNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+
+class MarkThreadAsSpam implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        private User $user,
+        private Thread $thread
+    ) {
+    }
+
+    public function handle()
+    {
+        $this->user->threadsMarkedAsSpammed()
+            ->attach($this->thread->getKey());
+
+        if ($this->shouldNotifyModerators()) {
+            Notification::send(
+                User::moderators()->get(),
+                new ThreadMarkedAsSpamNotification($this->thread)
+            );
+        }
+    }
+
+    private function shouldNotifyModerators()
+    {
+        return $this->thread->usersMarkingAsSpam()->count() >= 3;
+    }
+}
