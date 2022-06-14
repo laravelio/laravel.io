3e6f195417ee15ab9e32e4cdbca9c2c322b34949
sawirricardo <sawir.ricardo@gmail.com>
2022-06-14 13:58:56 +0700

    Create MarkThreadAsSpamTest.php

---
+++ tests/Integration/Jobs/MarkThreadAsSpamTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Jobs\MarkThreadAsSpam;
+use App\Models\Thread;
+use App\Models\User;
+use App\Notifications\ThreadMarkedAsSpamNotification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+use function Pest\Laravel\actingAs;
+
+uses(TestCase::class);
+uses(RefreshDatabase::class);
+
+it('can mark thread solution', function () {
+    $user = $this->login();
+    $thread = Thread::factory()->create();
+
+    $this->dispatch(new MarkThreadAsSpam($user, $thread));
+
+    $thread->refresh();
+
+    expect($thread->usersMarkingAsSpam()->count())->toBe(1);
+    expect($thread->usersMarkingAsSpam->contains($user))->toBeTrue();
+});
+
+it('can notify moderators if a thread is marked three times', function () {
+    Notification::fake();
+    $thread = Thread::factory()->create();
+    $users = User::factory(3)->create();
+    $moderator = User::factory()->create(['type' => User::MODERATOR]);
+
+    $users->each(function ($user, $index) use ($thread, $moderator) {
+        $this->dispatch(new MarkThreadAsSpam($user, $thread));
+        match ($index) {
+            2 => Notification::assertSentTo($moderator, ThreadMarkedAsSpamNotification::class),
+            default => Notification::assertNotSentTo($moderator, ThreadMarkedAsSpamNotification::class),
+        };
+    });
+});
