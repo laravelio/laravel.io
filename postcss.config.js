d0ddb23cafe0677096abb0fdd0fb3a90a824a889
Dries Vints <dries@vints.io>
2022-06-22 14:23:28 +0200

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        import: {},
-    },
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
