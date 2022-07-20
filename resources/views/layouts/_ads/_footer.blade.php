dfbd36e31d2ead3e565324fe1d442c7cbd2b3cf6
Dries Vints <dries@vints.io>
2022-07-20 18:26:34 +0200

    Update banner

--- resources/views/layouts/_ads/_footer.blade.php
+++ resources/views/layouts/_ads/_footer.blade.php
@@ -1,6 +1,6 @@
 @php($banner = Illuminate\Support\Arr::random(config('lio.ads')))
 
-<a href="{{ $banner['url'] }}" target="_blank" rel="noopener noreferrer" onclick="fathom.trackGoal('{{ $banner['goal'] }}', 0);">
+<a href="{{ $banner['long-url'] ?? $banner['url'] }}" target="_blank" rel="noopener noreferrer" onclick="fathom.trackGoal('{{ $banner['goal'] }}', 0);">
     {{-- Show the banner on bigger displays. --}}
     <img class="hidden md:block mx-auto w-full" style="max-width:1200px" src="{{ asset("/images/showcase/{$banner['image']}-long.png") }}" alt="{{ $banner['alt'] }}">
 
