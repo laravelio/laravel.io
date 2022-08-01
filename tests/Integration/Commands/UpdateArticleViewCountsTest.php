f0d9ec0b5d34777d8b314fe40f130c3b56db7c25
Joe Dixon <hello@joedixon.co.uk>
2022-08-01 21:43:16 +0100

    Merge article counts

--- tests/Integration/Commands/UpdateArticleViewCountsTest.php
+++ tests/Integration/Commands/UpdateArticleViewCountsTest.php
@@ -28,6 +28,46 @@ test('article view counts can be updated', function () {
     expect($article->fresh()->view_count)->toBe(1234);
 });
 
+test('article view counts can be merged with original url', function () {
+    Http::fake(function () {
+        return Http::response([[
+            'pageviews' => 1234,
+        ]]);
+    });
+
+    $article = Article::factory()->create([
+        'title' => 'My First Article',
+        'slug' => 'my-first-article',
+        'original_url' => 'https://example.com/my-first-article',
+        'submitted_at' => now(),
+        'approved_at' => now(),
+    ]);
+
+    (new UpdateArticleViewCounts)->handle();
+
+    expect($article->fresh()->view_count)->toBe(2468);
+});
+
+test('article view counts are not merged when url is invalid', function () {
+    Http::fake(function () {
+        return Http::response([[
+            'pageviews' => 1234,
+        ]]);
+    });
+
+    $article = Article::factory()->create([
+        'title' => 'My First Article',
+        'slug' => 'my-first-article',
+        'original_url' => 'erhwerhwerh',
+        'submitted_at' => now(),
+        'approved_at' => now(),
+    ]);
+
+    (new UpdateArticleViewCounts)->handle();
+
+    expect($article->fresh()->view_count)->toBe(1234);
+});
+
 test('article view counts are not updated if API call fails', function () {
     Http::fake(function () {
         return Http::response('Uh oh', 500);
