37921ee07b5e88e0d1f758aa2ed66fa4244f410a
Joe Dixon <hello@joedixon.co.uk>
2022-08-02 14:54:22 +0100

    Formatting

--- resources/views/layouts/_fathom.blade.php
+++ resources/views/layouts/_fathom.blade.php
@@ -1,5 +1,5 @@
 @env(['production', 'testing'])
     <!-- Fathom - beautiful, simple website analytics -->
-    <script src="https://boom.laravel.io/script.js" site="UXCUXOED" defer  @if(isset($canonical) && !Str::startsWith($canonical, config('app.url')))data-canonical="false"@endif></script>
+    <script src="https://boom.laravel.io/script.js" site="UXCUXOED" defer @if(isset($canonical) && !Str::startsWith($canonical, config('app.url')))data-canonical="false"@endif></script>
     <!-- / Fathom -->
 @endenv
