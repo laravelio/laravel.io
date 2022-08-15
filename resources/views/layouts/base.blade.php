4c9dd3aa03769e7a89e5c143127370af89322938
Rizwan3d <allgame45@hotmail.com>
2022-08-15 17:20:42 +0500

    Remove the use of an external css file

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,9 +17,6 @@
     <meta name="description" content="The Laravel portal for problem solving, knowledge sharing and community building." />
     <link rel="canonical" href="{{ $canonical ?? Request::url() }}" />
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('meta')
