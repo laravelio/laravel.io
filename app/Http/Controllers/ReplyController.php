6e4b7ffcc8181f8fdb1dd8726a17b3a30b410f0b
sawirricardo <sawir.ricardo@gmail.com>
2022-09-15 16:31:56 +0700

    reorder import

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Contracts\ReplyAble;
 use App\Http\Requests\CreateReplyRequest;
-use Illuminate\Http\Request;
 use App\Jobs\CreateReply;
 use App\Jobs\DeleteReply;
 use App\Jobs\ReportSpam;
