4e48b5dea1b5fd6774d12693c4da358e276cfb6f
Dries Vints <dries@vints.io>
2022-09-21 13:49:57 +0200

    wip

--- app/Models/SpamAble.php
+++ app/Models/SpamAble.php
@@ -2,17 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
-
-/**
- * This interface allows models to receive spam.
- *
- * @mixin \Illuminate\Database\Eloquent\Model
- */
 interface SpamAble
 {
     /**
-     * @return MorphToMany
+     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
      */
     public function spammers();
 }
