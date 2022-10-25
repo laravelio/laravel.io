268929c2b2744ae1c83d4bf810ff053e3dd909ab
Dries Vints <dries@vints.io>
2022-10-25 14:18:00 +0200

    Update link

--- config/lio.php
+++ config/lio.php
@@ -7,7 +7,7 @@ return [
         ['url' => 'https://fullstackeurope.com/2022?utm_source=Laravel.io&utm_campaign=fseu22&utm_medium=advertisement', 'image' => 'fseu', 'alt' => 'Full Stack Europe', 'goal' => 'SRIK2PEE'],
         [
             'url' => 'https://www.cloudways.com/en/php-hosting.php?id=972670',
-            'long-url' => 'https://www.cloudways.com/en/web-hosting-affiliate-program.php?id=972670&data1=cw_aff',
+            'long-url' => 'https://www.cloudways.com/en/bfcm-prepathon.php?id=972670',
             'image' => 'cloudways',
             'alt' => 'Cloudways',
             'goal' => '4KV6VZZ6',
