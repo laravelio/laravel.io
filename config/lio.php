c6bba7e4be16773426a677e65320eb498200bdce
Dries Vints <dries@vints.io>
2022-10-26 14:43:49 +0200

    wip

--- config/lio.php
+++ config/lio.php
@@ -4,7 +4,7 @@ return [
 
     'ads' => [
         ['url' => 'https://larajobs.com', 'image' => 'larajobs', 'alt' => 'LaraJobs', 'goal' => '9C3CAYKR'],
-        ['url' => 'https://fullstackeurope.com/2022?utm_source=Laravel.io&utm_campaign=fseu22&utm_medium=advertisement', 'image' => 'fseu', 'alt' => 'Full Stack Europe', 'goal' => 'SRIK2PEE'],
+        ['url' => 'https://fullstackeurope.com/2023?utm_source=Laravel.io&utm_campaign=fseu23&utm_medium=advertisement', 'image' => 'fseu', 'alt' => 'Full Stack Europe', 'goal' => 'SRIK2PEE'],
         [
             'url' => 'https://www.cloudways.com/en/php-hosting.php?id=972670',
             'long-url' => 'https://www.cloudways.com/en/bfcm-prepathon.php?id=972670',
