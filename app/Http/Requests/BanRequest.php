b8656f495d6f9b663d7489cd44428b79d57e89cb
Dries Vints <dries@vints.io>
2022-11-02 12:21:29 +0100

    wip

--- app/Http/Requests/BanRequest.php
+++ app/Http/Requests/BanRequest.php
@@ -14,7 +14,7 @@ class BanRequest extends FormRequest
     public function rules()
     {
         return [
-            'reason' => 'required|string|max:255',
+            'reason' => 'required|string',
         ];
     }
 
