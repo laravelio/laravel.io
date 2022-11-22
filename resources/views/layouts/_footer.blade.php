009b320fe12bf8939c52c927f0902f3542df8959
Benjamin Crozat <benjamincrozat@me.com>
2022-11-22 12:38:00 +0100

    Replaced empty link

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -7,9 +7,7 @@
         <div class="mx-4 md:mx-0">
             <div class="flex flex-col pb-8 mb-8 border-b lg:pb-16 border-gray-800 lg:flex-row">
                 <div class="w-full mb-6 lg:w-2/5 lg:pr-20 lg:mb-0">
-                    <a href="" class="block mb-5">
-                        <img loading="lazy" src="{{ asset('images/laravelio-logo-white.svg') }}" />
-                    </a>
+                    <img loading="lazy" src="{{ asset('images/laravelio-logo-white.svg') }}" alt="{{ config('app.name') }}" class="block mb-5" />
 
                     <p class="text-gray-100 lg:leading-loose">
                         The Laravel portal for problem solving, knowledge sharing and community building.
