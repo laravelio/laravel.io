1bccafbdd943b6707826d8c22d67e31d077f35fb
Dries Vints <dries@vints.io>
2022-12-11 16:01:35 +0100

    Update dependencies

---
+++ database/migrations/2022_12_11_145605_add_expires_at_column_sanctum.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->timestamp('expires_at')->nullable()->after('last_used_at');
+        });
+    }
+};
