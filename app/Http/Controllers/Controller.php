72e72df28c4c88b2c91cba79d58e25752e61c814
Joe Dixon <hello@joedixon.co.uk>
2023-09-05 21:08:04 +0100

    import trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Concerns\SendsAlerts;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
