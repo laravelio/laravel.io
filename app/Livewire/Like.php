dc410253d0531c2b50cf92ebabfb37ce1b677eda
faissaloux <fwahabali@gmail.com>
2024-01-04 15:51:04 +0100

    fix like component glitches

--- app/Livewire/Like.php
+++ app/Livewire/Like.php
@@ -12,13 +12,9 @@ final class Like extends Component
 
     public $type;
 
-    public $showLikers = false;
-
     private $likersLimit = 10;
 
     public $likers = '';
-
-    protected $listeners = ['toggleLikers'];
 
     public function mount($likable): void
     {
@@ -29,13 +25,6 @@ final class Like extends Component
 
         if (count($likers) > $this->likersLimit) {
             $this->likers .= ' and more';
-        }
-    }
-
-    public function toggleLikers(): void
-    {
-        if (strlen($this->likers)) {
-            $this->showLikers = ! $this->showLikers;
         }
     }
 
--- resources/views/livewire/like.blade.php
+++ resources/views/livewire/like.blade.php
@@ -1,7 +1,9 @@
-<div class="relative">
-    @if ($showLikers)
-        <x-tooltip>{{ $likers }} liked this {{ $type }}</x-tooltip>
+<div class="relative" x-data="{ showLikers: false }">
+    @if(strlen($likers))
+        <div x-cloak x-show="showLikers">
+            <x-tooltip>{{ $likers }} liked this {{ $type }}</x-tooltip>
+        </div>
     @endif
         
-    <x-heroicon-o-hand-thumb-up class="{{ $isSidebar ? 'w-6 h-6' : 'w-8 h-8' }}" wire:mouseover="toggleLikers" wire:mouseout="toggleLikers" />
+    <x-heroicon-o-hand-thumb-up class="{{ $isSidebar ? 'w-6 h-6' : 'w-8 h-8' }}" x-on:mouseover="showLikers = true" x-on:mouseout="showLikers = false" />
 </div>
