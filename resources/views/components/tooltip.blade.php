2ccf90d5ac4ab3c2a28fb44088ea0c433621c86a
faissaloux <fwahabali@gmail.com>
2024-01-04 03:19:11 +0100

    tooltip width

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -1,4 +1,4 @@
-<div class="absolute bottom-12 -left-3 text-white text-xs bg-gray-900 rounded-lg w-96 px-2 py-2">
+<div class="absolute bottom-12 -left-3 text-white text-xs bg-gray-900 rounded-lg w-max max-w-sm px-2 py-2">
     <div class="absolute -bottom-1 left-5 w-2 h-2 bg-gray-900 rotate-45"></div>
-    {{ $slot }}
+    <p class="w-full">{{ $slot }}</p>
 </div>
