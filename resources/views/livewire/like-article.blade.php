f281334b12f5519271422be28d7ba82d5e9ce0fa
faissaloux <fwahabali@gmail.com>
2024-01-04 02:16:48 +0100

    tooltip component

--- resources/views/livewire/like-article.blade.php
+++ resources/views/livewire/like-article.blade.php
@@ -1,9 +1,8 @@
 <div class="relative">
     @if ($showLikers)
-        <div class="absolute bottom-16 -left-3 text-white text-xs bg-gray-900 rounded-lg w-96 px-2 py-2">
-            <div class="absolute -bottom-1 left-5 w-2 h-2 bg-gray-900 rotate-45"></div>
+        <x-tooltip>
             {{ $likers }} liked this article
-        </div>
+        </x-tooltip>
     @endif
     @guest
         <div class="flex items-center text-gray-900 gap-x-4 gap-y-2.5 {{ $isSidebar ? 'flex-col' : 'flex-row' }}">
