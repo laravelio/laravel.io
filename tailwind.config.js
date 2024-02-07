81aa65d47bde7488a0cd1ae52fb86439357b3ddf
Emeka Mbah <frankemeks77@yahoo.com>
2024-02-07 22:39:13 +0000

    fix: Disable automatic dark mode
    Changelog: fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    darkMode: 'class',
     content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
