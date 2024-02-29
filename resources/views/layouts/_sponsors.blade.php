825fc7d9310fd5efaff107e78d93845355850b21
Dries Vints <dries@vints.be>
2024-02-29 12:18:42 +0100

    Revert "wip"
    This reverts commit 336178ed0bb8fe9bbaf1f66546e8f760f00e740d.

--- resources/views/layouts/_sponsors.blade.php
+++ resources/views/layouts/_sponsors.blade.php
@@ -40,6 +40,10 @@
             </div>
 
             <div class="col-span-2 flex justify-center lg:col-span-1">
+                <x-sponsor-logo url="https://steadfastcollective.com/uk-laravel-agency" logo="{{ asset('images/sponsors/steadfast.svg') }}" company="Steadfast Collective" />
+            </div>
+
+            <div class="col-span-2 flex justify-center lg:col-span-1">
                 <x-sponsor-logo url="https://www.bairesdev.com/sponsoring-open-source-projects/" logo="{{ asset('images/sponsors/bairesdev.png') }}" company="BairesDev" />
             </div>
 
