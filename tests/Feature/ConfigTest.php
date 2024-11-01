994ac12e2ca61cc56a8e96042d62e10f0ace05c3
Dries Vints <dries@vints.be>
2024-11-01 11:00:26 +0100

    wip

--- tests/Feature/ConfigTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Tests\TestCase;
-
-uses(TestCase::class);
-
-test('bcrypt hashing rounds reduced for faster test execution', function () {
-    $this->assertEquals(4, config('hashing.bcrypt.rounds'));
-});
