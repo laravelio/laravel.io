146bd0dcd4f49cd639f0455e536a4753fe22b77b
Dries Vints <dries@vints.be>
2025-05-19 21:10:31 +0200

    wip

--- app/Markdown/MarkdownServiceProvider.php
+++ app/Markdown/MarkdownServiceProvider.php
@@ -35,7 +35,7 @@ class MarkdownServiceProvider extends ServiceProvider
                 ],
                 'embed' => [
                     'adapter' => new OscaroteroEmbedAdapter,
-                    'allowed_domains' => ['youtube.com', 'twitter.com', 'x.com'],
+                    'allowed_domains' => ['youtube.com'],
                     'fallback' => 'link',
                 ],
             ]);
