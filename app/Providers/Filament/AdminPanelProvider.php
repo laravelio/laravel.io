2e233ee2d357350e99fcb90f761be652ae389c3b
theHocineSaad <saadhocine0@gmail.com>
2025-09-02 21:07:30 +0200

    Add widgets to the dashboards

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\ArticlesStatsOverview;
+use App\Filament\Widgets\ArticlesTrendChart;
+use App\Filament\Widgets\RepliesStatsOverview;
+use App\Filament\Widgets\RepliesTrendChart;
+use App\Filament\Widgets\UsersStatsOverview;
+use App\Filament\Widgets\UsersTrendChart;
 use Filament\Actions\Action;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -10,9 +16,6 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -37,8 +40,15 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                ArticlesStatsOverview::class,
+                UsersStatsOverview::class,
+                RepliesStatsOverview::class,
+                ArticlesTrendChart::class,
+                UsersTrendChart::class,
+                RepliesTrendChart::class,
+            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
